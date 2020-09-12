3f7f21913c716b04d05fb558b5b27bf71415b832
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-09-12 17:15:38 +0100

    Update guildManager.js

--- MainServer/guildManager.js
+++ MainServer/guildManager.js
@@ -1,11 +1,23 @@
 module.exports = async(client) => {
+  const configFile = require('../config.json')
+let LoggingChannel = client.channels.cache.get(configFile.LoggingChannel);
+
   client.on("guildCreate", guild => {
     // This event triggers when the bot joins a guild.
+    const embed = new Discord.MessageEmbed()
+    .setTitle(`Guild Removed`)
+    .setDescription(`**Guild Name:** ${guild.name}\n` + `**Guild ID:** ${guild.id}\n` + `**Guild Members:** ${guild.memberCount}\n` + `**Invite Link:** `)
+    .setTimestamp()
+    LoggingChannel.send(embed)
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   });
 
   client.on("guildDelete", guild => {
     // this event triggers when the bot is removed from a guild.
-    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    const embed = new Discord.MessageEmbed()
+    .setTitle(`Guild Added`)
+    .setDescription(`**Guild Name:** ${guild.name}\n` + `**Guild ID:** ${guild.id}`)
+    .setTimestamp()
+    LoggingChannel.send(embed)
   });
 }
