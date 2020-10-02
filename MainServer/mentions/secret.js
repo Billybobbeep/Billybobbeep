d96f7e961b9ad1e3cc495e8218e591382b6c0d39
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-02 18:34:17 +0100

    Create secret.js

---
+++ MainServer/mentions/secret.js
@@ -0,0 +1,8 @@
+module.exports = async (client, message, args) => {
+    if (!args[2]) {
+    embed.setDescription('Please specify a message to send');
+    return message.channel.send(embed)
+    }
+    await message.delete();
+    await message.channel.send('||' + args.slice(2).join(" ") + '||');
+}
