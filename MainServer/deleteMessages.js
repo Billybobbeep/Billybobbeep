235add8edb3219fd5a63dfcc8632cd9c9707de03
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-03 09:00:15 +0100

    Update deleteMessages.js

--- MainServer/deleteMessages.js
+++ MainServer/deleteMessages.js
@@ -1,3 +1,5 @@
+const editMessage = require('./editMessage');
+
 module.exports = async client => {
 	const Discord = require(`discord.js`);
 	const configFile = require('../config.json');
@@ -14,22 +16,6 @@ module.exports = async client => {
 	client.on('messageDelete', message => {
 		let msg = message.content.toLowerCase();
 
-		if (message.guild.id !== '733442092667502613') return;
-		if (message.author.bot) return;
-		if (msg.startsWith(prefix + `purge`)) return;
-		if (message.content === null) {
-				embed.setDescription(
-        `**Content:** *This message provided no text.*\n` +
-        `**Message ID:** ${message.id}\n` +
-        `**Channel:** ${message.channel}\n\n` +
-        `**Author:** ${message.author}\n` +
-        `**Author Tag:** ${message.author.tag}\n` +
-        `**Author ID:** ${message.author.id}\n\n` +
-        `**Command:** ${command}\n` +
-        `**Pinned:** ${pinned}\n`)
-			LoggingChannel.send(embed);
-		}
-
 		if (msg.startsWith(prefix)) {
 			command = true;
 		} else {
@@ -39,6 +25,48 @@ module.exports = async client => {
 			pinned = true;
 		} else {
 			pinned = false;
+		}
+
+		if (message.guild.id != configFile.ServerId) return;
+		if (message.author.bot) return;
+		if (msg.startsWith(prefix + `purge`)) return;
+		if (message.content === null || message.content === " " || message.content === undefined) {
+			embed.setDescription(
+			`**Content:** *This message provided no text.*\n` +
+			`**Message ID:** ${message.id}\n` +
+			`**Channel:** ${message.channel}\n\n` +
+			`**Author:** ${message.author}\n` +
+			`**Author Tag:** ${message.author.tag}\n` +
+			`**Author ID:** ${message.author.id}\n\n` +
+			`**Command:** ${command}\n` +
+			`**Pinned:** ${pinned}\n`)
+			return LoggingChannel.send(embed);
+		}
+		
+		if (message.attachments.size > 0) {
+			embed.setDescription(
+			`**Content:** *This message contained an image.*\n` +
+			`**Message ID:** ${message.id}\n` +
+			`**Channel:** ${message.channel}\n\n` +
+			`**Author:** ${message.author}\n` +
+			`**Author Tag:** ${message.author.tag}\n` +
+			`**Author ID:** ${message.author.id}\n\n` +
+			`**Command:** ${command}\n` +
+			`**Pinned:** ${pinned}\n`)
+			return LoggingChannel.send(embed);
+		}
+		if (message.embeds.length > 0) {
+			embed.setDescription(
+				`**Content:** *This message contained an embeded link.*\n` +
+				`**Link**: ${message.content}` +
+				`**Message ID:** ${message.id}\n` +
+				`**Channel:** ${message.channel}\n\n` +
+				`**Author:** ${message.author}\n` +
+				`**Author Tag:** ${message.author.tag}\n` +
+				`**Author ID:** ${message.author.id}\n\n` +
+				`**Command:** ${command}\n` +
+				`**Pinned:** ${pinned}\n`)
+				return LoggingChannel.send(embed);
 		}
 
 			embed.setDescription(
