c15f06075efb649f8928e5b0b8d407a824f79c8f
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-03 15:45:30 +0100

    Update credit.js

--- Embeds/credit.js
+++ Embeds/credit.js
@@ -10,7 +10,7 @@ module.exports = async(client, msg, args, prefix, message) => {
     "Scripted by: **Spoink#2793** & **Will Os#9857**\n" +
     "Name development & suggestions: **wibbleywobbley♡#1564**\n\n" +
     "It has been a huge pleasure to script & develop Billybobbeep and a great thanks for all the support I have received throughout the development processes. However, Billybobbeep is not finished yet! We are working towards the brand-new version 2.0." +
-    "\n\n If you would like to view more, feel free to check out our website: https://billybobbeep-1.tyler2p.repl.co/home")
+    "\n\n If you would like to view more, feel free to check out our website: https://billybobbeep.tyler2p.repl.co/home")
     embed.setColor("7aa0f6")
     embed.setFooter(`Requested by: ${message.author.tag}`)
     embed.setTimestamp()
