f6490cc4dcce515e96c660b1120114b8f54a8f03
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-04 16:29:46 +0100

    Added anti spam functions

--- MainServer/antiSpam/antiSpam.js
+++ MainServer/antiSpam/antiSpam.js
@@ -1,19 +1,39 @@
+const Discord = require('discord.js');
+const embed = new Discord.MessageEmbed()
+const settings = require('./settings.json');
+const configFile = require('../../config.json');
 var AntiSpam = []
-//var options {
-//    "ignoreBots" : false
-//}
+var count = 0;
 
 module.exports = async (client) => {
 
+    client.on('message', async message => {
+        count = 0
+        if (!message.guild || message.author.id === message.client.user.id || settings.ignoreBots && message.author.bot ) return;
+        if (message.channel.id === message.author.lastMessageChannelID) {
+            AntiSpam.push(message.author.id)
+        }
+        AntiSpam.forEach((result) => {
+            if (message.author.id === result) {
+                count++;
+            }
+        });
+        if (count === 5) {
+            embed.setTitle('Billybobbeep | Spam Prevention');
+            embed.setDescription('You have been warned for spamming in ' + message.guild.name);
+            embed.setColor('#fbc2eb')
+            embed.setTimestamp()
+            await message.author.send(embed)
+            message.member.roles.add(configFile.MutedRole)
+            setTimeout(() => {
+                message.member.roles.remove(configFile.MutedRole)
+            }, 1000);
+        }
+    });
+    setInterval(() => {
+        if (AntiSpam.length < settings.messagesToWarn) return;
+        AntiSpam.forEach((result) => {
+            AntiSpam.splice(AntiSpam.indexOf(result), 1);
+        });
+    }, settings.timeToWarn);
 }
-
- /*   client.on('message', (message) => {
-        if (
-			!message.guild ||
-			message.author.id === message.client.user.id ||
-			message.guild.ownerID === message.author.id ||
-			options.ignoreBots && message.author.bot
-		) {
-			return false
-    }); 
-}*/
