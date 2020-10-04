97390383baaf8900c38630408d1759bc6311192c
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-04 12:55:44 +0100

    Create antiSpam.js

---
+++ MainServer/antiSpam/antiSpam.js
@@ -0,0 +1,19 @@
+var AntiSpam = []
+//var options {
+//    "ignoreBots" : false
+//}
+
+module.exports = async (client) => {
+
+}
+
+ /*   client.on('message', (message) => {
+        if (
+			!message.guild ||
+			message.author.id === message.client.user.id ||
+			message.guild.ownerID === message.author.id ||
+			options.ignoreBots && message.author.bot
+		) {
+			return false
+    }); 
+}*/
