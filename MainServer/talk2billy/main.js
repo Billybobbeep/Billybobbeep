7533f862540081018b2c0ec2b6f01d04037a6186
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-04 12:55:26 +0100

    Update main.js

--- MainServer/talk2billy/main.js
+++ MainServer/talk2billy/main.js
@@ -1,16 +1,33 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+const modules = require('./modules.js');
+const configFile = require('../../config.json');
+const settings = require('./settings.json');
+const fs = require('fs');
 
 module.exports = async (client) => {
-    const configFile = require('../../config.json')
-    if (configFile.talk2billyEnabled === true) {
-
+    if (configFile.talk2billyEnabled != settings.talk2billyEnabled) {
+        fs.readFile('./MainServer/talk2billy/settings.json', 'utf8', function readFileCallback(err, data) {
+            nd = JSON.parse(data);
+            fs.writeFile('./MainServer/talk2billy/settings.json', `{ "talk2billyEnabled" : ${configFile.talk2billyEnabled}, "testCmd" : ${nd.testCmd} }`, 'utf8', function() { } );
+        });
+        }
+    if (settings.testCmd === false || settings.testCmd === true) {
+        client.on('message', message => {
+            if (message.content.toLowerCase() === '--test--' || message.content.toLowerCase() === '--test-- on' || message.content.toLowerCase() === '--test-- true') {
+                if (settings.testCmd === true) return message.channel.send('Test mode is already on.')
+                return modules.testCmdOn(message)
+            } else if (message.content.toLowerCase() === '--test-- off' || message.content.toLowerCase() === '--test-- false') {
+                if (settings.testCmd === false) return message.channel.send('Test mode is already off.')
+                return modules.testCmdOff(message)
+            }
+        });
+    }
+    if (settings.talk2billyEnabled === true && settings.testCmd === false) {
+        var file = require('./message.js')
+        file(client)
     } else {
         client.on('message', async message => {
-            if (message.channel.id != configFile.talk2billy) return;
-            const embed = new Discord.MessageEmbed()
-            .setTitle('Billybobbeep | Talk to Billy')
-            .setDescription('Talk to Billy has been disabled.')
-            message.channel.send(embed)
+            modules.disabled(message)
         });
     }
 }
