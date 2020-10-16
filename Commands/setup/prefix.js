ea445bab3f6646879bfef015acff5c1ec35d86f4
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-16 18:12:13 +0100

    Updated setup prefix

--- Commands/setup/prefix.js
+++ Commands/setup/prefix.js
@@ -1,16 +1,17 @@
 const { MessageEmbed } = require('discord.js')
-function helpEmbed(message, db) {
+function helpEmbed(message, db, prefix) {
   const embed = new MessageEmbed()
-  .setTitle('Setup | Prefix')
-  .setDescription('')
-  .setColor()
+  .setTitle('Billybobbeep | Setup Command')
+  .setDescription(`With this command you can change the servers default prefix.\n**Usage**\`${prefix}setup prefix [newPrefix]\``)
+  .setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`)
+  message.channel.send(embed)
 }
 
 module.exports = (message, db, prefix, args) => {
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('You need the `Administrator` premissions to run this command.')
   var newPrefix = args[0]
   if (!newPrefix && db.get(message.guild.id + '.prefix') === '~') {
-    helpEmbed(message, db)
+    helpEmbed(message, db, prefix)
   }
   if (!newPrefix && !db.get(message.guild.id + '.prefix')) return message.channel.send('Please specify a prefix.')
   if (!newPrefix) {
