21ddcb09b2d93b3e466db245e22151efb5bf450f
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-17 15:53:02 +0100

    Updated talk2billy modules

--- MainServer/talk2billy/main.js
+++ MainServer/talk2billy/main.js
@@ -1,29 +1,13 @@
-const Discord = require('discord.js');
-const modules = require('./modules.js');
-const configFile = require('../../config.json');
-const settings = require('./settings.json');
-const fs = require('fs');
+const db = require('quick.db');
+const fetch = require('isomorphic-fetch');
 
 module.exports = async (client, message) => {
-  if (configFile.talk2billyEnabled != settings.talk2billyEnabled) {
-    fs.readFile('./MainServer/talk2billy/settings.json', 'utf8', function readFileCallback(err, data) {
-      nd = JSON.parse(data);
-      fs.writeFile('./MainServer/talk2billy/settings.json', `{ "talk2billyEnabled" : ${configFile.talk2billyEnabled}, "testCmd" : ${nd.testCmd} }`, 'utf8', function() { });
-    });
-  }
-  if (settings.testCmd === false || settings.testCmd === true) {
-    if (message.content.toLowerCase() === '--test--' || message.content.toLowerCase() === '--test-- on' || message.content.toLowerCase() === '--test-- true') {
-      if (settings.testCmd === true) return message.channel.send('Test mode is already on.')
-      return modules.testCmdOn(message)
-    } else if (message.content.toLowerCase() === '--test-- off' || message.content.toLowerCase() === '--test-- false') {
-      if (settings.testCmd === false) return message.channel.send('Test mode is already off.')
-      return modules.testCmdOff(message)
-    }
-  }
-  if (settings.talk2billyEnabled === true && settings.testCmd === false) {
-    var file = require('./message.js')
-    file(client, message)
-  } else {
-    modules.disabled(message)
+  if (!message.guild) return;
+  let talk2billy = db.get(message.guild.id + '.talk2billy')
+  if (talk2billy) {
+    if (message.channel.id !== talk2billy) return;
+    const response = await fetch('https://some-random-api.ml/chatbot?message=' + args[0].replace(' ', '%20'));
+    const text = await response.text();
+    message.channel.send(text.replace('response', '').replace(':', '').replace('{', '').replace('}', '').replace('??', '?').replace('???', '?').replace('""', '').replace('"', '').replace('"', ''));
   }
 }
