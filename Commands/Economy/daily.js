8df16cf3f3f12336083c47bd13df3d9792dae64d
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-18 15:26:23 +0100

    Updated economy commands

--- Commands/Economy/daily.js
+++ Commands/Economy/daily.js
@@ -2,9 +2,8 @@ const Discord = require(`discord.js`);
 const db = require('quick.db');
 const ms = require('ms');
 
-module.exports = async (prefix, message) => {
+module.exports = async (prefix, message, client) => {
   const embed = new Discord.MessageEmbed();
-  embed.setTimestamp();
   embed.setTitle('Billybobbeep | Economy');
   embed.setFooter(`${message.author.username}`);
   embed.setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`);
@@ -52,55 +51,40 @@ module.exports = async (prefix, message) => {
   let doctor = db.get(message.author.id + '.jobs.doctor') || undefined;
 
   if (jobs !== undefined) {
-    if (cashier !== undefined) {
-      dailyAmt = 7.25
-    }
-    if (teacher !== undefined) {
-      dailyAmt = 8.81
-    }
-    if (waiter !== undefined) {
-      dailyAmt = 9.50
-    }
-    if (receptionist !== undefined) {
-      dailyAmt = 9.58
-    }
-    if (architect !== undefined) {
-      dailyAmt = 8.66
-    }
-    if (lifeGuard !== undefined) {
-      dailyAmt = 11.40
-    }
-    if (nurse !== undefined) {
-      dailyAmt = 17.52
-    }
-    if (police !== undefined) {
-      dailyAmt = 17.61
-    }
-    if (engineer !== undefined) {
-      dailyAmt = 20.55
-    }
-    if (cheif !== undefined) {
-      dailyAmt = 20.76
-    }
-    if (clinicalScientist !== undefined) {
-      dailyAmt = 22.45
-    }
-    if (headScientist !== undefined) {
-      dailyAmt = 22.70
-    }
-    if (lawyer !== undefined) {
-      dailyAmt = 25.61
-    }
-    if (socialWorker !== undefined) {
-      dailyAmt = 27.71
-    }
-    if (doctor !== undefined) {
-      dailyAmt = 52.25
-    }
+    if (cashier !== undefined) dailyAmt = 7.25
+    if (teacher !== undefined) dailyAmt = 8.81
+    if (waiter !== undefined) dailyAmt = 9.50
+    if (receptionist !== undefined) dailyAmt = 9.58
+    if (architect !== undefined) dailyAmt = 8.66
+    if (lifeGuard !== undefined) dailyAmt = 11.40
+    if (nurse !== undefined) dailyAmt = 17.52
+    if (police !== undefined) dailyAmt = 17.61
+    if (engineer !== undefined) dailyAmt = 20.55
+    if (cheif !== undefined) dailyAmt = 20.76
+    if (clinicalScientist !== undefined) dailyAmt = 22.45
+    if (headScientist !== undefined) dailyAmt = 22.70
+    if (lawyer !== undefined) dailyAmt = 25.61
+    if (socialWorker !== undefined) dailyAmt = 27.71
+    if (doctor !== undefined) dailyAmt = 52.25
+    if (streak === 5) dailtAmt = 75
   }
-  
+  let nem = client.emojis.cache.get('767351869856940063');
+  let sem = client.emojis.cache.get('767365396474101831');
+  let semoji = `${nem}${nem}${nem}${nem}${nem}`
+  if (streak === 1 || streak === 0) {
+    semoji = `${sem}${nem}${nem}${nem}${nem}`
+  } else if (streak === 2) { semoji = `${sem}${sem}${nem}${nem}${nem}`
+  } else if (streak === 3) { semoji = `${sem}${sem}${sem}${nem}${nem}`
+  } else if (streak === 4) { semoji = `${sem}${sem} ${sem}${sem}${nem}`
+  } else if (streak === 5) { semoji = `${sem}${sem}${sem}${sem}${sem}`
+    embed.setDescription(`You have collected your **${dailyAmt}** reward.\n\n**__Daily streak progress__**\n${semoji}\n\n`);
+    embed.setFooter(`Total Streak: ${streak}\nWallet: ${balance + dailyAmt}`);
+    message.channel.send(embed);
+    return db.delete(message.author.id + '.economy.streak');
+  }
+
   if (lastRun !== null && cooldown - (Date.now() - lastRun) > 0) {
-    embed.setDescription(`You have already collected your daily allowance today.\nTime Left: \`${timeObj}\``);
+    embed.setDescription(`You have already collected your daily allowance today.\nTime Left: **${timeObj}**`);
     message.channel.send(embed);
   } else {
     db.add(message.author.id + '.economy.balance', dailyAmt);
@@ -108,7 +92,8 @@ module.exports = async (prefix, message) => {
     db.add(message.author.id + '.economy.streak', 1)
 
     //db.delete(message.author.id);
-    embed.setDescription(`I have added \`${dailyAmt}\` onto your account balance.\nYour current day streak is \`${streak}\`.\n\nYou now have \`${balance + dailyAmt}\` in your wallet.`);
+    embed.setDescription(`I have added **${dailyAmt}** onto your account balance.\n\n**__Daily streak progress__**\n${semoji}\n\n`);
+    embed.setFooter(`Total Streak: ${streak}\nWallet: ${balance + dailyAmt}`)
     message.channel.send(embed);
   }
 }
--- Commands/Economy/work.js
+++ Commands/Economy/work.js
@@ -2,7 +2,7 @@ const Discord = require(`discord.js`);
 const db = require('quick.db');
 const ms = require('ms');
 
-module.exports = async (prefix, message) => {
+module.exports = async (prefix, message, client) => {
   const embed = new Discord.MessageEmbed();
   embed.setTimestamp();
   embed.setFooter(`${message.author.username}`);
@@ -47,64 +47,35 @@ module.exports = async (prefix, message) => {
   let doctor = db.get(message.author.id + '.jobs.doctor') || undefined;
 
   if (jobs !== undefined) {
-    if (cashier !== undefined) {
-      workAmt = 10
-    }
-    if (teacher !== undefined) {
-      workAmt = 11
-    }
-    if (waiter !== undefined) {
-      workAmt = 12
-    }
-    if (receptionist !== undefined) {
-      workAmt = 12
-    }
-    if (architect !== undefined) {
-      workAmt = 15
-    }
-    if (lifeGuard !== undefined) {
-      workAmt = 16
-    }
-    if (nurse !== undefined) {
-      workAmt = 21
-    }
-    if (police !== undefined) {
-      workAmt = 22
-    }
-    if (engineer !== undefined) {
-      workAmt = 24
-    }
-    if (cheif !== undefined) {
-      workAmt = 25
-    }
-    if (clinicalScientist !== undefined) {
-      workAmt = 25
-    }
-    if (headScientist !== undefined) {
-      workAmt = 26
-    }
-    if (lawyer !== undefined) {
-      workAmt = 29
-    }
-    if (socialWorker !== undefined) {
-      workAmt = 31
-    }
-    if (doctor !== undefined) {
-      workAmt = 55
-    }
+    if (cashier !== undefined) workAmt = 10
+    if (teacher !== undefined) workAmt = 11
+    if (waiter !== undefined) workAmt = 12
+    if (receptionist !== undefined) workAmt = 12
+    if (architect !== undefined) workAmt = 15
+    if (lifeGuard !== undefined) workAmt = 16
+    if (nurse !== undefined) workAmt = 21
+    if (police !== undefined) workAmt = 22
+    if (engineer !== undefined) workAmt = 24
+    if (cheif !== undefined) workAmt = 25
+    if (clinicalScientist !== undefined) workAmt = 25
+    if (headScientist !== undefined) workAmt = 26
+    if (lawyer !== undefined) workAmt = 29
+    if (socialWorker !== undefined) workAmt = 31
+    if (doctor !== undefined) workAmt = 55
   }
-  if (workAmt === undefined) {
+  /*if (workAmt === undefined) {
     embed.setDescription(`Before you can start working, you need to get a job.\n\nTo Apply for a job use '${prefix}jobs'`);
     message.channel.send(embed)
-  } else if (Date.now() < lastRun) {
+  } else */if (Date.now() < lastRun) {
     var seco = 'seconds'
     let time2work = ms(Date.now() - lastRun);
     time2work = time2work.replace('-', '');
     if (time2work.endsWith('ms')) time2work = '1s';
     if (time2work === '1s') seco = 'second';
     time2work.replace('s', '')
+    let crossEmoji = client.emojis.cache.get('736952985330122772');
 
-    embed.setDescription(`Break Time. You can work again in **${time2work}** ${seco}.`);
+    embed.setDescription(`${crossEmoji} Break Time. You can work again in **${time2work}** ${seco}.`);
     message.channel.send(embed);
   } else {
     //db.add(message.author.id + '.economy.balance', workAmt);
