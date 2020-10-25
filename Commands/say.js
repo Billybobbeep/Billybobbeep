2ddaa6866ee610e69b0619f9535c7e90e9f82edd
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-25 11:03:31 +0000

    Prevented errors in say command

--- Commands/say.js
+++ Commands/say.js
@@ -3,6 +3,7 @@ const configFile = require('../config.json');
 
 module.exports = async(client, msg, args, prefix, message) => {    
     let wantToSay = args.join(" ");
+    if (!args[0]) return message.channel.send('You must specify a message to send.');
     message.channel.send(wantToSay);
     message.delete();
 }
