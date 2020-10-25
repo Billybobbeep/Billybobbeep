c6699d90d2c394676188129befcdf2fdb9374592
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-25 11:03:18 +0000

    Fixed loopholes on secret commands

--- Commands/secret.js
+++ Commands/secret.js
@@ -3,6 +3,9 @@ const configFile = require('../config.json');
 
 module.exports = async (client, msg, args, prefix, message) => {
   let secretMessage = args.join(" ");
+  if (message.content.includes('||')) return message.channel.send('You cannot include `||` in your message.');
+  if (message.content.toLowerCase().includes('/spoiler')) return message.chanel.send('You cannot include `/spoiler` in your message.');
+  if (!args[0]) return message.channel.send('You must specify a message to send.');
   message.channel.send(`||${secretMessage}||`);
   message.delete();
 }
