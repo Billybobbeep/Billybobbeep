a4ac6baa3be413a9976e83f818217133c14177f8
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-25 18:26:39 +0000

    Update purge.js

--- Commands/purge.js
+++ Commands/purge.js
@@ -7,42 +7,37 @@ module.exports = async (client, msg, args, prefix, message) => {
     if (isNaN(args[0])) return message.channel.send("You have entered an invalid number.")
     if (args[0] > 100) return message.channel.send("Please enter a number below 100.")
     if (args[0] < 2) return message.channel.send("Please enter a number more than 1.")
-
+    var msg1;
     message.delete()
     message.channel.bulkDelete(args[0])
-      .then(messages =>
-        message.channel.send(`Deleted ${messages.size}/${args[0]} messages.`))
-      .then(d =>
-        d.delete({ timeout: 3000 }))
-      .catch(() => message.channel.send("Something went wrong, please ensure the messages are not over 2 weeks old."));
+      .then(messages => {
+        async function main() {
+          msg1 = await message.channel.send(`Deleted ${messages.size}/${args[0]} messages.`);
 
-    let LoggingChannel = client.channels.cache.get(db.get(message.guild.id + '.loggingChannel'));
+          const embed = new Discord.MessageEmbed()
+          embed.setTitle(`Purged Messages`)
+          embed.setDescription(`**Channel:** ${message.channel}\n` +
+                              `**Messages Purged:** ${messages.size}\n\n` +
+                              `**Moderator:** ${message.author}\n` +
+                              `**Moderator Tag:** ${message.author.tag}\n` +
+                              `**Moderator ID:** ${message.author.id}\n`)
+          embed.setTimestamp()
+          embed.setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`)
+          let LoggingChannel = client.channels.cache.get(db.get(message.guild.id + '.loggingChannel'));
 
-    let pinned;
-    if (message.pinned) {
-      pinned = true
-    } else {
-      pinned = false
-    }
+          if (LoggingChannel) {
+            try {
+              LoggingChannel.send(embed)
+            } catch {
+              return;
+            }
+          }
 
-    const embed = new Discord.MessageEmbed()
-      .setTitle(`Purged Messages`)
-      .setDescription(`**Channel:** ${message.channel}
-                        **Messages Purged:** ${args[0]}\n
-                        **Moderator:** ${message.author}
-                        **Moderator Tag:** ${message.author.tag}
-                        **Moderator ID:** ${message.author.id}\n
-                        **Command:** true
-                        **Pinned:** ${pinned}`)
-      .setTimestamp()
-      .setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`)
-    if (LoggingChannel) {
-      try {
-        LoggingChannel.send(embed)
-      } catch {
-        return;
-      }
-    }
+          msg1.delete({ timeout: 3000 })
+        }
+        main()
+      })
+      .catch((error) => console.log(error))//message.channel.send("Something went wrong, please ensure the messages are not over 2 weeks old."));
   }
   var debounce = false;
 
