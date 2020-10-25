afbd4c636d5cab2d21964a516cae4ac490c1e307
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-25 18:26:46 +0000

    Update counting.js

--- MainServer/counting.js
+++ MainServer/counting.js
@@ -8,12 +8,13 @@ module.exports = (client, message) => {
     if (isNaN(message.content)) return;
     if (!currentNo.get(message.guild.id)) currentNo.add(message.guild.id, '0');
     var curr = currentNo.get(message.guild.id);
-    if (message.content === curr + '1') {
+    console.log(message.content)
+    if (message.content === curr + 1) {
         message.react(TE)
-        currentNo.add(message.guild.id, '1');
+        currentNo.add(message.guild.id, 1);
     } else {
-        currentNo.set(message.guild.id, '0');
+        currentNo.set(message.guild.id, 0);
         message.react(CE)
-        message.reply('has ruined the chain with an incorrect number.\nThe next number is `1`.');
+        message.reply('has ruined the chain with an incorrect number.\nThe next number is `' + currentNo.get(message.guild.id) + '`.');
     }
 }
