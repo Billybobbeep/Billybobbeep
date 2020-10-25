12e4b3c146fc682bf795d3af9c9476cee135eb50
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-25 10:17:59 +0000

    Improved formatting

--- MainServer/deleteMessages.js
+++ MainServer/deleteMessages.js
@@ -14,86 +14,86 @@ module.exports = async client => {
   embed.setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`);
   let LoggingChannel = client.channels.cache.get(db.get(message.guild.id + '.loggingChannel'));
   let prefix = db.get(message.guild.id + '.prefix') || '~'
-    let msg = message.content.toLowerCase();
+  let msg = message.content.toLowerCase();
 
-    if (msg.startsWith(prefix)) {
-      command = true;
-    } else {
-      command = false;
-    }
-    if (message.pinned) {
-      pinned = true;
-    } else {
-      pinned = false;
-    }
+  if (msg.startsWith(prefix)) {
+    command = true;
+  } else {
+    command = false;
+  }
+  if (message.pinned) {
+    pinned = true;
+  } else {
+    pinned = false;
+  }
 
-    if (message.author.bot) return;
-    if (msg.startsWith(prefix + `purge`)) return;
-    if (message.content === null || message.content === " " || message.content === undefined) {
-      embed.setDescription(
-        `**Content:** *This message provided no text.*\n` +
-        `**Message ID:** ${message.id}\n` +
-        `**Channel:** ${message.channel}\n\n` +
-        `**Author:** ${message.author}\n` +
-        `**Author Tag:** ${message.author.tag}\n` +
-        `**Author ID:** ${message.author.id}\n\n` +
-        `**Command:** ${command}\n` +
-        `**Pinned:** ${pinned}\n`)
-        try {
-          return LoggingChannel.send(embed);
-        } catch {
-          return;
-        }
-    }
-
-    if (message.attachments.size > 0) {
-      embed.setDescription(
-        `**Content:** *This message contained an image.*\n` +
-        `**Message ID:** ${message.id}\n` +
-        `**Channel:** ${message.channel}\n\n` +
-        `**Author:** ${message.author}\n` +
-        `**Author Tag:** ${message.author.tag}\n` +
-        `**Author ID:** ${message.author.id}\n\n` +
-        `**Command:** ${command}\n` +
-        `**Pinned:** ${pinned}\n`)
-        try {
-          return LoggingChannel.send(embed);
-        } catch {
-          return;
-        }
-    }
-    if (message.content.toLowerCase().includes('https://') || message.content.toLowerCase().includes('http://') || message.content.toLowerCase().includes('www.') || message.content.toLowerCase().includes('.com') || message.content.toLowerCase().includes('.co.uk')) {
-      embed.setDescription(
-        `**Content:** *This message contained an embeded link.*\n` +
-        `**Link**: ${message.content}\n` +
-        `**Message ID:** ${message.id}\n` +
-        `**Channel:** ${message.channel}\n\n` +
-        `**Author:** ${message.author}\n` +
-        `**Author Tag:** ${message.author.tag}\n` +
-        `**Author ID:** ${message.author.id}\n\n` +
-        `**Command:** ${command}\n` +
-        `**Pinned:** ${pinned}\n`)
-        try {
-          return LoggingChannel.send(embed);
-      } catch {
-          return;
-        }
-    }
-
+  if (message.author.bot) return;
+  if (msg.startsWith(prefix + `purge`)) return;
+  if (message.content === null || message.content === " " || message.content === undefined) {
     embed.setDescription(
-      '**Content:** ' + message.content +
-      '\n**Message ID:** ' + message.id +
-      '\n**Channel:** ' + message.channel +
-      `\n\n**Author:** ${message.author}` +
-      '\n**Author Tag:** ' + message.author.tag +
-      '\n**Author ID:** ' + message.author.id +
-      '\n\n**Command:** ' + command +
-      '\n**Pinned:** ' + pinned
-    )
-    try {
-      LoggingChannel.send(embed);
+      `**Content:** *This message provided no text.*\n` +
+      `**Message ID:** ${message.id}\n` +
+      `**Channel:** ${message.channel}\n\n` +
+      `**Author:** ${message.author}\n` +
+      `**Author Tag:** ${message.author.tag}\n` +
+      `**Author ID:** ${message.author.id}\n\n` +
+      `**Command:** ${command}\n` +
+      `**Pinned:** ${pinned}\n`)
+      try {
+        return LoggingChannel.send(embed);
+      } catch {
+        return;
+      }
+  }
+
+  if (message.attachments.size > 0) {
+    embed.setDescription(
+      `**Content:** *This message contained an image.*\n` +
+      `**Message ID:** ${message.id}\n` +
+      `**Channel:** ${message.channel}\n\n` +
+      `**Author:** ${message.author}\n` +
+      `**Author Tag:** ${message.author.tag}\n` +
+      `**Author ID:** ${message.author.id}\n\n` +
+      `**Command:** ${command}\n` +
+      `**Pinned:** ${pinned}\n`)
+      try {
+        return LoggingChannel.send(embed);
+      } catch {
+        return;
+      }
+  }
+  if (message.content.toLowerCase().includes('https://') || message.content.toLowerCase().includes('http://') || message.content.toLowerCase().includes('www.') || message.content.toLowerCase().includes('.com') || message.content.toLowerCase().includes('.co.uk')) {
+    embed.setDescription(
+      `**Content:** *This message contained an embeded link.*\n` +
+      `**Link**: ${message.content}\n` +
+      `**Message ID:** ${message.id}\n` +
+      `**Channel:** ${message.channel}\n\n` +
+      `**Author:** ${message.author}\n` +
+      `**Author Tag:** ${message.author.tag}\n` +
+      `**Author ID:** ${message.author.id}\n\n` +
+      `**Command:** ${command}\n` +
+      `**Pinned:** ${pinned}\n`)
+      try {
+        return LoggingChannel.send(embed);
     } catch {
-      return;
-    }
-  });
+        return;
+      }
+  }
+
+  embed.setDescription(
+    '**Content:** ' + message.content +
+    '\n**Message ID:** ' + message.id +
+    '\n**Channel:** ' + message.channel +
+    `\n\n**Author:** ${message.author}` +
+    '\n**Author Tag:** ' + message.author.tag +
+    '\n**Author ID:** ' + message.author.id +
+    '\n\n**Command:** ' + command +
+    '\n**Pinned:** ' + pinned
+  )
+  try {
+    LoggingChannel.send(embed);
+  } catch {
+    return;
+  }
+});
 };
