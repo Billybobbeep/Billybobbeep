4823026963c55965c8d12b226fd30aad28aee359
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-26 19:28:32 +0000

    Improved mute command

--- Commands/mute.js
+++ Commands/mute.js
@@ -4,26 +4,25 @@ var embed = new Discord.MessageEmbed();
 
 module.exports = async(client, msg, args, prefix, message) => {
   async function muteCmd() {
-    let user = message.mentions.users.first() || message.guild.members.cache.get(args[1])
+    let user = message.mentions.users.first() || message.guild.members.cache.get(args[0])
     let reason = args.slice(1).join(" ");
 
     if (!db.get(message.guild.id + '.mutedRole')) {
       return message.channel.send('Please setup a muted role in your server to use this command.')
     }
     if (!user) {
-      return message.channel.send('Please mention a user to mute.')
+      return message.channel.send('Please specify a user to mute.')
     }
-    if (!reason) {
-      reason = 'No reason was provided'
-    }
-    let member = message.guild.member(user);
-    embed.setTitle('You have been muted')
+    if (user.id === message.author.id)return message.channel.send('You cannot mute yourself.')
+    if (!reason) return message.channel.send('Please specify a reason.')
     embed.setTimestamp()
     embed.setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`)
-    embed.setDescription(`You have been muted `)
-    user.send('You have been muted in ' + message.guild.name + '\n**Reason:** ' + reason)
+    embed.setDescription(`You have been muted in ${message.guild.name}\nReason: ${reason}`)
+    await user.send(embed)
     if (!message.guild.roles.fetch(r => r.id === db.get(message.guild.id + '.mutedRole')))
-    member.roles.add(db.get(message.guild.id + '.mutedRole'))
+    message.guild.member.cache
+      .find(member => member.id === user.id)
+      .roles.add(db.get(message.guild.id + '.mutedRole'));
     message.channel.send('Successfully muted <@!' + user + '>.')
     
     if (db.get(message.guild.id + '.loggingChannel')) {
@@ -31,7 +30,7 @@ module.exports = async(client, msg, args, prefix, message) => {
       embed.setTitle('User Muted')
       embed.setTimestamp()
       embed.setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`)
-      embed.setDescription()
+      embed.setDescription(`**User:** ${user}\n**User Tag:** ${user.tag}\n**User ID:** ${user.id}\n\n**Moderator:** ${message.author}\n**Moderator Tag:** ${message.author.tag}\n**Moderator ID:** ${message.author.id}`)
       try {
         LoggingChannel.send(embed)
       } catch {
