e06e4bb0c225f6cdbab98fe449a577efafd047bb
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-10-28 14:17:58 +0000

    General update

--- Commands/members.js
+++ Commands/members.js
@@ -4,25 +4,23 @@ const db = require('../databaseManager/index.js');
 
 module.exports = async(client, msg, args, prefix, message) => {
 let member = message.guild.members;
-let offline = member.cache.filter(m => m.user.presence.status === "offline").size,
-    on = member.cache.filter(m => m.user.presence.status === "online").size,
-    idle = member.cache.filter(m => m.user.presence.status === "idle").size,
-    dnd = member.cache.filter(m => m.user.presence.status === "dnd").size,
+let offline = member.cache.filter(m => m.user.presence.status === 'offline').size,
+    online = member.cache.filter(m => m.user.presence.status === 'online').size,
+    idle = member.cache.filter(m => m.user.presence.status === 'idle').size,
+    dnd = member.cache.filter(m => m.user.presence.status === 'dnd').size,
     robot = member.cache.filter(m => m.user.bot).size,
     total = message.guild.memberCount;
-    online = on - robot - 1;
-    if (msg.startsWith(prefix + "members")) {
-        const memberEmbed = new Discord.MessageEmbed()
-        .setTitle("Billybobbeep | Server Members")
-        .addField("Total Members:", `${total}`, true)
-        .addField("Offline Members:", `${offline}`, true)
-        .addField("Online Members:", `${online}`, true)
-        .addField("Idle Members:", `${idle}`, true)
-        .addField("DND Members:", `${dnd}`, true)
-        .addField("Bots:", `${robot}`, true)
-        .setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`)
-        .setFooter(`Requested by: ${message.author.tag}`)
-        .setTimestamp()
-        message.channel.send(memberEmbed)
-    }
+    
+    const embed = new Discord.MessageEmbed()
+    .setTitle('Billybobbeep | Server Members')
+    .addField('Total Members:', `${total}`, true)
+    .addField('Offline Members:', `${offline}`, true)
+    .addField('Online Members:', `${online}`, true)
+    .addField('Idle Members:', `${idle}`, true)
+    .addField('DND Members:', `${dnd}`, true)
+    .addField('Bots:', `${robot}`, true)
+    .setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`)
+    .setFooter(`Requested by: ${message.author.tag}`)
+    .setTimestamp()
+    message.channel.send(embed)
 }
--- MainServer/timeOut.js
+++ MainServer/timeOut.js
@@ -2,7 +2,7 @@ const ms = require('ms');
 module.exports = (db, client) => {
     setInterval(() => {
         mute(db, client)
-    }, 60000);
+    }, 20);
 }
 
 function mute(db, client) {
@@ -13,7 +13,6 @@ function mute(db, client) {
 
     if (!MM) return;
     if (MM.length < 1) return;
-    console.log(MM)
 
     MM.forEach(result => {
         result = result.replace('_', ' ').replace('_', ' ');
@@ -24,26 +23,32 @@ function mute(db, client) {
         
         guild = client.guilds.cache.get(guild)
         let member = guild.members.cache.get(user);
-        if (!member) return remove(MM, db, guild, user, time, client);
+        if (!member) return remove(MM, db, guild, user, time, client, 'mute');
         if (member.roles.cache.find(role => role.id === db.get(guild.id + db.get(guild + '.mutedRole')))) {
             if (Date.now() > ms(time)) {
-                remove(MM, db, guild, user, time, client);
+                remove(MM, db, guild, user, time, client, 'mute');
             }
         } else {
-            remove(MM, db, guild, user, time, client);
+            remove(MM, db, guild, user, time, client, 'mute');
         }
     });
 }
 
-function remove(table, db, guild, user, time, client) {
-    let clientGuild = client.guilds.cache.get(guild);
-    let member = guild.members.cache.get(user);
+function remove(table, db, guild, user, time, client, string) {
+    let member = guild.members.cache.find(m => m.id === user)
+    console.log(user)
+    console.log(member)
     console.log(table)
     table.splice(table.indexOf(guild.id.toString() + '_' + user.toString() + '_' + time.toString()), 1);
     db.set('mutedMembers', table);
     console.log(table);
-    if (!member) return;
-    if (member.roles.cache.find(role => role.id === db.get(guild.id + db.get(guild.id + '.mutedRole')))) {
-        member.roles.remove(clientGuild.roles.cache.find(role => role.id === db.get(guild.id + '.mutedRole')));
+    if (string === 'mute') {
+        console.log('mute')
+        if (!member) return;
+        console.log('mute1')
+        if (member.roles.cache.find(role => role.id === db.get(guild.id + db.get(guild.id + '.mutedRole')))) {
+            console.log('mute2')
+            member.roles.remove(guild.roles.cache.find(role => role.id === db.get(guild.id + '.mutedRole')));
+        }
     }
 }
