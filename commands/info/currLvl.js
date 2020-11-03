de8df3cf7aae688f72abb811ba33c1b97e7efefe
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-11-03 10:26:32 +0000

    Updated commands
    Added levelling commands to the command modules

---
+++ commands/info/currLvl.js
@@ -0,0 +1,26 @@
+module.exports = {
+  name: 'currlvl',
+  description: 'View a users current level.',
+  alias: ['cl'],
+  guildOnly: true,
+  execute(message, prefix, client) {
+    const db = require('../../data/databaseManager/index.js');
+    const Discord = require('discord.js');
+
+    var prefix = db.get(message.guild.id + '.prefix') || '~'
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let user = message.guild.members.cache.get(args[1]) || message.mentions.users.first() || message.author
+    let currLvl = db.get(message.guild.id + '_' + user.id + '.level') || 0;
+
+    if (user.username === undefined) {
+      user = user.user
+    }
+    const embed = new Discord.MessageEmbed()
+    embed.setTitle('Billybobbeep | Levelling System')
+    embed.setTimestamp()
+    embed.setFooter(`Requested by: ${message.author.tag}`)
+    embed.setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`)
+    embed.setDescription(`${user.username}'s level is currently ${currLvl}`)
+    message.channel.send(embed)
+  }
+}
---
+++ commands/misc/removeLvl.js
@@ -0,0 +1,36 @@
+module.exports = {
+  name: 'removelvl',
+  description: 'Remove a users level.',
+  alias: ['rl', 'removelevel'],
+  guildOnly: true,
+  execute(message, prefix, client) {
+    const db = require('../../data/databaseManager/index.js');
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let user = message.guild.members.cache.get(args[1]) || message.mentions.users.first();
+    if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have premission to run this command.')
+
+    if (!user)
+      return message.channel.send('Please mention a user.');
+
+    let currLvl = db.get(message.guild.id + '_' + user.id + '.level');
+    if (!args[2])
+      return message.channel.send('Please specify an amount.');
+
+    if (args[2] === 'all' || args[2] === 'a') {
+      db.delete(message.guild.id + '_' + user.id + '.level');
+      return message.channel.send(`Cleared ${user}'s levels.`)
+    }
+    if (isNaN(args[2])) 
+      return message.channel.send('You have entered an invalid amount');
+
+    let amount = args[2]
+    if (amount > currLvl)
+      return message.channel.send(`${user} does not have ${amount} level(s)`);
+
+    if (amount < 1)
+      return message.channel.send('You hae entered an invalid amount');
+
+    db.subtract(message.guild.id + '_' + user.id + '.level', amount)
+    message.channel.send(`removed ${amount} level(s) from ${user}`)
+  }
+}
