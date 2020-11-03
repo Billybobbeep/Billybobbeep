7b2aaa56aca2af8cc45c0039f8cdf03cc909b435
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-11-03 17:09:45 +0000

    Updated bot activity

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
-    let activities = [`~help`, `Version 2.0 BETA`]
+    let activities = [`~help`, `Version ${require('../../package-lock.json').version}`]
       i = 0;
     setInterval(() => {
       client.user.setActivity(`${activities[i++ % activities.length]}`, {type: 'LISTENING'});
