486a5ecee2a4c92c5bf98dfa052e60b28a66ce05
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-11-04 16:57:11 +0000

    Update logging.js

--- events/backend/logging.js
+++ events/backend/logging.js
@@ -34,36 +34,38 @@ module.exports.add = (guild, client) => {
 }
 
 module.exports.remove = (guild, client) => {
-    guild.fetchAuditLogs()
-    .then(logs => {
-        let ban = logs.entries
-        .filter(e => e.action === 'MEMBER_BAN_REMOVE')
-        .sort((a, b) => b.createdAt - a.createdAt)
-        .first();
-        let pb = logs.entries
-        .filter(e => e.action === 'MEMBER_BAN_ADD')
-        .filter(e => e.target.id === ban.target.id)
-        .sort((a, b) => b.createdAt - a.createdAt)
-        .first();
+    setTimeout(() => {
+        guild.fetchAuditLogs()
+        .then(logs => {
+            let ban = logs.entries
+            .filter(e => e.action === 'MEMBER_BAN_REMOVE')
+            .sort((a, b) => b.createdAt - a.createdAt)
+            .first();
+            let pb = logs.entries
+            .filter(e => e.action === 'MEMBER_BAN_ADD')
+            .filter(e => e.target.id === ban.target.id)
+            .sort((a, b) => b.createdAt - a.createdAt)
+            .first();
 
-        let LoggingChannel = client.channels.cache.get(db.get(guild.id + '.loggingChannel'));
-        if (LoggingChannel) {
-            embed.setTitle('User Unbanned');
-            embed.setDescription(
-            `**User Tag:** ${ban.target.tag}\n` +
-            `**User ID:** ${ban.target.id}\n\n` +
-            `**Banned For:** ${pb.reason || 'No reason was provided.'}\n\n` +
-            `**Moderator:** ${ban.executor}\n` +
-            `**Moderator Tag:** ${ban.executor.tag}\n` +
-            `**Moderator ID:** ${ban.executor.id}\n`
-            )
-            embed.setTimestamp(ban.createdTimestamp);
-            embed.setColor(`${db.get(guild.id + '.embedColor') || '#447ba1'}`);
-            try {
-                LoggingChannel.send(embed)
-            } catch {
-                return;
+            let LoggingChannel = client.channels.cache.get(db.get(guild.id + '.loggingChannel'));
+            if (LoggingChannel) {
+                embed.setTitle('User Unbanned');
+                embed.setDescription(
+                `**User Tag:** ${ban.target.tag}\n` +
+                `**User ID:** ${ban.target.id}\n\n` +
+                `**Banned For:** ${pb.reason || 'No reason was provided.'}\n\n` +
+                `**Moderator:** ${ban.executor}\n` +
+                `**Moderator Tag:** ${ban.executor.tag}\n` +
+                `**Moderator ID:** ${ban.executor.id}\n`
+                )
+                embed.setTimestamp(ban.createdTimestamp);
+                embed.setColor(`${db.get(guild.id + '.embedColor') || '#447ba1'}`);
+                try {
+                    LoggingChannel.send(embed)
+                } catch {
+                    return;
+                }
             }
-        }
-    });
+        });
+    }, 100);
 }
