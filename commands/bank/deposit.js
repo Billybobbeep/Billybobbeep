41c922ac04caa8fccf9778eb06cc11c09fea2a2f
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-11-07 13:14:41 +0000

    Bug improvements
    Fixed loopholes & bugs with the banking commands

--- commands/bank/deposit.js
+++ commands/bank/deposit.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'deposit',
     description: 'Deposit some money into your bank account.',
+    alias: ['dep'],
     guildOnly: true,
     execute (message, prefix, client) {
         const db = require('../../data/databaseManager/index.js');
@@ -10,20 +11,20 @@ module.exports = {
         let args = message.content.slice(prefix.length).trim().split(/ +/g);
 
         if (!args[1]) return message.channel.send(`You need to specify an amount.`);
-        if (args[1] !== 'a' || args[1] !== 'all') {
-            let amt = args[1].replace('$', '');
-            if (isNaN(amt)) return message.channel.send(`${amt} is not a valid amount.`);
-            if (db.get(message.author.id + '.economy.balance') < amt || !db.get(message.author.id + '.economy.balance')) return message.channel.send(`You do not have **${amt}** in your wallet.`);
-
-            db.subtract(message.author.id + '.economy.balance', amt);
-            db.add(message.author.id + '.bank.balance', amt);
-            message.channel.send(`Successfully deposited **${amt}**.`);
-        } else {
+        if (args[1] === 'a' || args[1] === 'all') {
             if (db.get(message.author.id + '.economy.balance') < 1) return message.channel.send(`You do not have any cash to deposit.`);
             let amt = db.get(message.author.id + '.economy.balance');
             db.add(message.author.id + '.bank.balance', db.get(message.author.id + '.economy.balance'));
             db.subtract(message.author.id + '.economy.balance', amt);
             message.channel.send(`Successfully deposited all of your cash.`);
+        } else {
+            let amt = args[1].replace('$', '');
+            if (isNaN(amt)) return message.channel.send(`**${amt}** is not a valid amount.`);
+            if (db.get(message.author.id + '.economy.balance') < amt || !db.get(message.author.id + '.economy.balance')) return message.channel.send(`You do not have **$${amt}** in your wallet.`);
+
+            db.subtract(message.author.id + '.economy.balance', amt);
+            db.add(message.author.id + '.bank.balance', amt);
+            message.channel.send(`Successfully deposited **$${amt}**.`);
         }
     }
 }
