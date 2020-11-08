307a16df44e8c06bb5c6b3010a430fe8952599c7
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-11-08 20:36:12 +0000

    Added job requirements
    Added job requirements to one json formatted javascript file

---
+++ commands/Economy/jobRequirements.js
@@ -0,0 +1,92 @@
+module.exports = {
+    cashier: {
+        level: 0,
+        promotions: 5,
+        pay: 10,
+        description:'A starter minimum wage job.'
+    },
+    teacher: {
+        level: 3,
+        promotions: 3,
+        pay: 11,
+        description:'Prepare lessons for children.'
+    },
+    waiter: {
+        level: 7,
+        promotions: 5,
+        pay: 12,
+        description:'Suggest food & beverages to customers.'
+    },
+    receptionist: {
+        level: 15,
+        promotions: 7,
+        pay: 12,
+        description:'Welcome, assist and direct visitors correctly.'
+    },
+    architect: {
+        level: 25,
+        promotions: 2,
+        pay: 15,
+        description:'To develop new building designs.'
+    },
+    lifeguard: {
+        level: 40,
+        promotions: 4,
+        pay: 16,
+        description:'To supervise all individuals using the swimming pool and for the safe activities around.'
+    },
+    nurse: {
+        level: 55,
+        promotions: 7,
+        pay: 21,
+        description:'Provide care and treatment to patients.'
+    },
+    police: {
+        level: 60,
+        promotions: 10,
+        pay: 22,
+        description:'Participate in raids & arrests.'
+    },
+    engineer: {
+        level: 75,
+        promotions: 2,
+        pay: 24,
+        description:'Fix any vehicle that comes in for a repair.'
+    },
+    chief: {
+        level: 90,
+        promotions: 15,
+        pay: 25,
+        description:'Cook and prepare meals as requested.'
+    },
+    clinicalScientist: {
+        level: 105,
+        promotions: 3,
+        pay: 25,
+        description:'Research different parts of the body.'
+    },
+    headScientist: {
+        level: 110,
+        promotions: 3,
+        pay: 26,
+        description:'Research different parts of the body.'
+    },
+    lawyer: {
+        level: 130,
+        promotions: 5,
+        pay: 29,
+        description:'Show and explain different ways a person is or isn\'t guilty.'
+    },
+    socialWorker: {
+        level: 155,
+        promotions: 0,
+        pay: 31,
+        description:'Give good and clear advice on certain problems a person may have.'
+    },
+    doctor: {
+        level: 180,
+        promotions: 20,
+        pay: 55,
+        description:'Treat and care for patients.'
+    }
+}
