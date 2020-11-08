b7a050072bf66af5e6de9dfb267470e56d437b02
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-11-08 20:33:54 +0000

    Update dmRecieving.js
    Lettings users know that we record these messages when first time dming billy

--- events/backend/dmRecieving.js
+++ events/backend/dmRecieving.js
@@ -3,10 +3,11 @@ module.exports = async (message, client) => {
   const configFile = require('../../structure/config.json');
   if (message.channel.type !== 'dm') return;
   if (message.author.bot) return;
+  if (message.author.id === client.user.id) return;
   const db = require('../../data/databaseManager/index.js');
   const logging = require('../../utils/functions.js').logging;
 
-  const embed = new Discord.MessageEmbed();
+  var embed = new Discord.MessageEmbed();
   embed.setTitle(`DM Recieved`);
   embed.setDescription(
     `**Content:** ${message}\n` +
@@ -17,6 +18,15 @@ module.exports = async (message, client) => {
   embed.setTimestamp();
   embed.setColor(`${db.get('733442092667502613.embedColor') || '#447ba1'}`);
 
-  if (message.content.toLowerCase().startsWith('~')) return;
+  if (!message.content.toLowerCase().startsWith('~'))
   logging(embed, message, client)
+
+  if (!db.get(message.author.id + '.dmed')) {
+    embed = new Discord.MessageEmbed();
+    embed.setDescription(`Please remember that all messages sent to billybobbeep are recorded.`);
+    embed.setAuthor(message.author.username, message.author.displayAvatarURL());
+    embed.setColor(`${db.get('733442092667502613.embedColor') || '#447ba1'}`);
+    message.channel.send(embed);
+    db.set(message.author.id + '.dmed', true);
+  }
 }
