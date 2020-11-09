988c5a69676d9c6d1ac6741f47103ce5a2021726
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-11-09 19:32:27 +0000

    Fixed cooldown
    Fixed bugs with cooldown error when donating

--- commands/Economy/donate.js
+++ commands/Economy/donate.js
@@ -10,8 +10,8 @@ module.exports = {
         let args = message.content.slice(prefix.length).trim().split(/ +/g);
         if (db.get(message.guild.id + '.ecoEnabled') === false) return message.channel.send(`Economy have been disabled for this server.`);
         let user = message.mentions.users.first() || message.guild.members.cache.get(args[1]);
-        if (!user.id) user = user.user;
         if (!user) return message.channel.send(`You have not mentioned a user.`);
+        if (!user.id) user = user.user;
 
         let emoji = client.emojis.cache.get(require('../../structure/config.json').blobSmile);
         if (user.id === client.user.id) return message.channel.send(`I do not need money, thanks though. ${emoji}`)
@@ -22,19 +22,23 @@ module.exports = {
         let amt = args[2];
         if (isNaN(amt)) return message.channel.send(`Please enter a valid number to donate.`);
         if (amt > db.get(message.author.id + '.economy.balance')) return message.channel.send(`You do not have **${amt}** in your wallet.`);
-        message.channel.send(`Donated **${amt}** to <@!${user.id}>.`);
 
         if (db.get(message.author.id + '.economy.lastDonated')) {
             let lastRun = db.get(message.author.id + '.economy.lastDonated');
-            if (Date.now() < lastRun) {
-                let time = ms(Date.now() - lastRun);
+            if (Date.now() < (lastRun + 300000)) {
+                let time = ms(Date.now() - (lastRun + 300000));
                 time = time.replace('-', '');
-                return message.channel.send(`You have to wait ${time} before donating again.`);
+                if (time.endsWith('ms')) time = '1s';
+                if (time.endsWith('s') && time !== '1s') time = time.replace('s', ' seconds');
+                if (time.endsWith('s') && time === '1s') time = time.replace('s', ' second');
+                if (time.endsWith('m')) time = time.replace('m', ' minutes');
+                return message.channel.send(`You have to wait **${time}** before donating again.`);
             }
         }
 
+        db.set(message.author.id + '.economy.lastDonated', Date.now());
+        message.channel.send(`Donated **${amt}** to <@!${user.id}>.`);
         db.subtract(message.author.id + '.economy.balance', amt);
         db.add(user.id + '.economy.balance', amt);
-        db.set(message.author.id + '.economy.lastDonated', Date.now());
     }
 }
