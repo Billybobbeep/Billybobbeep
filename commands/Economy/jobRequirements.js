009b18635d01971f3b3f981fd6bcf8efa5b5390e
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-11-09 16:41:31 +0000

    Update jobRequirements.js
    Added more data to the module

--- commands/Economy/jobRequirements.js
+++ commands/Economy/jobRequirements.js
@@ -1,92 +1,120 @@
 module.exports = {
+    global: {
+        xp: {
+            lower: {
+                max: 80 //xp for level
+            },
+            higher: {
+                max: 130 //xp for level
+            }
+        },
+        work: {
+            cooldown: 3000 //cooldown in milliseconds between each work shift
+        }
+    },
     cashier: {
         level: 0,
         promotions: 5,
         pay: 10,
-        description:'A starter minimum wage job.'
+        description:'A starter minimum wage job.',
+        xp : 5
     },
     teacher: {
         level: 3,
         promotions: 3,
         pay: 11,
-        description:'Prepare lessons for children.'
+        description:'Prepare lessons for children.',
+        xp : 3
     },
     waiter: {
         level: 7,
         promotions: 5,
         pay: 12,
-        description:'Suggest food & beverages to customers.'
+        description:'Suggest food & beverages to customers.',
+        xp : 3
     },
     receptionist: {
         level: 15,
         promotions: 7,
         pay: 12,
-        description:'Welcome, assist and direct visitors correctly.'
+        description:'Welcome, assist and direct visitors correctly.',
+        xp : 5
     },
     architect: {
         level: 25,
         promotions: 2,
         pay: 15,
-        description:'To develop new building designs.'
+        description:'To develop new building designs.',
+        xp : 60
     },
     lifeguard: {
         level: 40,
         promotions: 4,
         pay: 16,
-        description:'To supervise all individuals using the swimming pool and for the safe activities around.'
+        description:'To supervise all individuals using the swimming pool and for the safe activities around.',
+        xp : 5
     },
     nurse: {
         level: 55,
         promotions: 7,
         pay: 21,
-        description:'Provide care and treatment to patients.'
+        description:'Provide care and treatment to patients.',
+        xp : 6
     },
     police: {
         level: 60,
         promotions: 10,
         pay: 22,
-        description:'Participate in raids & arrests.'
+        description:'Participate in raids & arrests.',
+        xp : 7
     },
     engineer: {
         level: 75,
         promotions: 2,
         pay: 24,
-        description:'Fix any vehicle that comes in for a repair.'
+        description:'Fix any vehicle that comes in for a repair.',
+        xp : 3
     },
     chief: {
         level: 90,
         promotions: 15,
         pay: 25,
-        description:'Cook and prepare meals as requested.'
+        description:'Cook and prepare meals as requested.',
+        xp : 7
     },
     clinicalScientist: {
         level: 105,
         promotions: 3,
         pay: 25,
-        description:'Research different parts of the body.'
+        description:'Research different parts of the body.',
+        xp : 3
     },
     headScientist: {
         level: 110,
         promotions: 3,
         pay: 26,
-        description:'Research different parts of the body.'
+        description:'Research different parts of the body.',
+        xp : 3
     },
     lawyer: {
         level: 130,
         promotions: 5,
         pay: 29,
-        description:'Show and explain different ways a person is or isn\'t guilty.'
+        description:'Show and explain different ways a person is or isn\'t guilty.',
+        xp : 5
     },
     socialWorker: {
         level: 155,
         promotions: 0,
         pay: 31,
-        description:'Give good and clear advice on certain problems a person may have.'
+        description:'Give good and clear advice on certain problems a person may have.',
+        xp : 5
     },
     doctor: {
         level: 180,
         promotions: 20,
         pay: 55,
-        description:'Treat and care for patients.'
+        description:'Treat and care for patients.',
+        xp : 5
     }
 }
