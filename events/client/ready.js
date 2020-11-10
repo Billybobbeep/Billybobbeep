3d3de6f7ced1a57da294b4aa900524e08764d30a
Tyler2P <68140692+Tyler2P@users.noreply.github.com>
2020-11-10 15:58:48 +0000

    Update ready.js
    Activity update

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,6 @@
 module.exports = (client) => {
-    let activities = [`~help`, `Version ${require('../../package-lock.json').version}`]
+    //let activities = [`~help`, `Version ${require('../../package-lock.json').version}`];
+    let activities = [`Billianist and proud`];
       i = 0;
     setInterval(() => {
       client.user.setActivity(`${activities[i++ % activities.length]}`, {type: 'LISTENING'});
@@ -13,7 +14,7 @@ module.exports.reconnecting = (client) => {
   let channel = guild.channels.cache.get('733595548577300542');
   let user = guild.members.cache.get(require('../../structure/config.json').SpoinkID);
   try {
-    user.send(`Billybobbeep cannot connect`);
+    user.send(`${client.user.username} cannot connect.`);
   } catch {
     channel.send(`<@!${user.id}> Please turn your DMs on.`);
   }
