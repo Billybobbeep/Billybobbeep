7ef81b89e5fa270f2139fa4bdcdbc566c2ceae92
Tyler <tylerrp06@gmail.com>
2020-11-28 18:19:02 +0000

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -11,8 +11,8 @@ module.exports = {
     const logging = require('../../utils/functions.js').logging;
     let args = message.content.slice(prefix.length).trim().split(/ +/g);
     async function muteCmd() {
-      let user = message.mentions.users.first() || message.guild.members.cache.get(args[1])
-      let reason = args.slice(3).join(" ");
+      let user = message.mentions.users.first() || message.guild.members.cache.get(args[1]);
+      let reason = args.slice(3).join(' ');
       let time = args[2] || undefined;
       let member = message.guild.members.cache.get(user.id);
       if (member.roles.cache.find(r => r.id === db.get(message.guild.id + '.mutedRole'))) return message.channel.send(`<@!${user.id}> is already muted.`);
@@ -24,14 +24,22 @@ module.exports = {
       if (!reason) return message.channel.send('Please specify a reason.');
       if (!member) return message.channel.send(`I could not find the member you provided.`);
       if (time === undefined) return message.channel.send('Please specify a time.');
-      if (time.endsWith('h') || time.endsWith('m')) time = ms(time); else return message.channel.send('The time can only be in hours or minutes.');
+      try {
+        if (time.endsWith('h') || time.endsWith('m')) time = ms(time); else return message.channel.send('The time can only be in hours or minutes.');
+      } catch {
+        return message.channel.send('Please specify a time');
+      }
       embed1.setTimestamp()
       embed1.setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`)
       embed1.setTitle('You have been muted');
       embed1.addField(`Responsible Moderator:`, message.author.tag);
       embed1.addField(`Reason:`, reason);
       embed1.addField(`Guild:`, message.guild.name);
-      embed1.addField(`Time:`, ms(time).replace('m', ' minute(s)').replace('h', ' hours'));
+      try {
+        embed1.addField(`Time:`, ms(time).replace('m', ' minute(s)').replace('h', ' hours'));
+      } catch {
+        return message.channel.send('Please specify a time');
+      }
       try {
         await user.send(embed1)
       } catch {
@@ -51,7 +59,7 @@ module.exports = {
 
     var debounce = false;
 
-    if (message.member.hasPermission("MANAGE_MESSAGES") || message.member.hasPermission("ADMINISTRATOR")) {
+    if (message.member.hasPermission('MANAGE_MESSAGES') || message.member.hasPermission('ADMINISTRATOR')) {
       muteCmd()
       debounce = true;
     } else if (db.get(message.guild.id + '.modRole')) {
