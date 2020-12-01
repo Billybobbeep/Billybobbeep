128d2f91f88606fba140a510535c4a8f36ded186
Tyler <tylerrp06@gmail.com>
2020-12-01 19:37:22 +0000

    Added rank command
    Added a new rank command to see your level stats

--- commands/info/rank.js
+++ commands/info/rank.js
@@ -1,3 +1,5 @@
+const { Canvas } = require('canvas');
+
 module.exports = {
     name: 'rank',
     description: 'View your xp.',
@@ -6,39 +8,49 @@ module.exports = {
     guildOnly: true,
 
     async execute (message, prefix, client) {
-        const Discord = require('discord.js');
-        const canvacord = require('canvacord');
         const db = require('../../structure/global').db;
+        const canvas = require('canvas');
         let args = message.content.slice(prefix.length).trim().split(/ +/g);
         let user = message.mentions.users.first() || message.guild.members.cache.get(args[1]) || message.author;
-        if (!user.displayAvatarURL() || !user.id) user = user.user;
+        if (!user.username) user = user.user;
         let displayName = `${user.username[0].toUpperCase()}${(user.username).substring(1).toLowerCase()}`;
-        let avatar = await canvacord.Canvas.trigger(user.displayAvatarURL({ dynamic: false, format: 'png' }));
+        let avatar = await canvas.loadImage(user.displayAvatarURL({ dynamic: false, format: 'png' }));
         var msg = await message.channel.send('Please wait...');
 
-        var rank = new canvacord.Rank();
-        rank.setAvatar(avatar);
-        rank.setUsername(displayName.toString());
-        rank.setDiscriminator(user.discriminator);
-        rank.setStatus((user.presence.status).toString());
-        rank.setCurrentXP(db.get(message.guild.id + '_' + user.id + '.xp') || 0);
-        rank.setRequiredXP(500)
-        rank.setLevel(db.get(message.guild.id + '_' + user.id + '.level') || 0);
-        try {
-            rank.build().then(data => {
-                const attachment = new Discord.MessageAttachment(data);
-                message.channel.send(attachment);
-                msg.delete();
-            });
-        } catch {
-            if (user.id === message.author.id) {
-                message.channel.send(`You do not have a rank yet`);
-                msg.delete();
-            } else {
-                message.channel.send(`${displayName} does not have a rank yet`);
-                msg.delete();
-            }
-            return;
+        var level = db.get(message.guild.id + '_' + user.id + '.level') || 0;
+        var xp = db.get(message.guild.id + '_' + user.id + '.xp') || 1;
+        var discriminator = user.discriminator;
+
+        let xpForLevel = 30;
+        var currlev = db.get(message.guild.id + '_' + message.author.id + '.level');
+        if (currlev > 20 && currlev < 30) {
+            xpForLevel = 35
+        } else if (currlev >= 30 && currlev < 40) {
+            xpForLevel = 40
+        } else if (currlev >= 40 && currlev < 50) {
+            xpForLevel = 45
+        } else if (currlev >= 50 && currlev < 60) {
+            xpForLevel = 50
+        } else if (currlev >= 70 && currlev < 80) {
+            xpForLevel = 55
+        } else if (currlev >= 90 && currlev < 100) {
+            xpForLevel = 60
+        } else if (currlev >= 100 && currlev < 110) {
+            xpForLevel = 100
+        } else if (currlev >= 110 && currlev < 120) {
+            xpForLevel = 110
+        } else if (currlev >= 120 && currlev < 140) {
+            xpForLevel = 120
+        } else if (currlev >= 140 && currlev < 160) {
+            xpForLevel = 130
+        } else if (currlev >= 160 && currlev < 180) {
+            xpForLevel = 140
+        } else if (currlev >= 180) {
+            xpForLevel = 150
         }
+
+        require('../../utils/functions').rank(message, avatar, displayName, Number(discriminator), xp, xpForLevel, level);
+        
+        msg.delete();
     }
 }
--- utils/functions.js
+++ utils/functions.js
@@ -25,20 +25,224 @@ module.exports.logging = function(msg, message, client, option) {
     }
 }
 
-module.exports.rank = function(avatar, username, discriminator, currentXP, requiredXP, level) {
-    const canvas = require('canvas');
+module.exports.rank = async function(message, avatar, username, discriminator, currentXP, requiredXP, level) {
+    var canvas = require('canvas');
+    const Discord = require('discord.js');
+    const assets = require('@canvacord/assets');
+
+    this.data = {
+        background: {
+            type: "color",
+            image: "#23272A"
+        },
+        overlay: {
+            display: true,
+            level: 0.5,
+            color: "#333640"
+        },
+        progressBar: {
+            rounded: true,
+            x: 275.5,
+            y: 183.75,
+            height: 37.5,
+            width: 596.5,
+            track: {
+                color: "#484b4E"
+            },
+            bar: {
+                type: "color",
+                color: "#FFFFFF"
+            }
+        },
+        avatar: {
+            source: null,
+            x: 70,
+            y: 50,
+            height: 180,
+            width: 180
+        },
+        level: {
+            display: true,
+            data: 1,
+            textColor: "#FFFFFF",
+            color: "#F3F3F3",
+            displayText: "LEVEL"
+        },
+        currentXP: {
+            data: 0,
+            color: "#FFFFFF"
+        },
+        requiredXP: {
+            data: 0,
+            color: "#FFFFFF"
+        },
+        discriminator: {
+            discrim: null,
+            color: "rgba(255, 255, 255, 0.4)"
+        },
+        username: {
+            name: null,
+            color: "#FFFFFF"
+        },
+    };
+    
+    function abbrev(num) {
+        if (!num || isNaN(num)) return "0";
+        if (typeof num === "string") num = parseInt(num);
+        let decPlaces = Math.pow(10, 1);
+        var abbrev = ["K", "M", "B", "T"];
+        for (var i = abbrev.length - 1; i >= 0; i--) {
+            var size = Math.pow(10, (i + 1) * 3);
+            if (size <= num) {
+                num = Math.round((num * decPlaces) / size) / decPlaces;
+                if (num == 1000 && i < abbrev.length - 1) {
+                    num = 1;
+                    i++;
+                }
+                num += abbrev[i];
+                break;
+            }
+        }
+        return num;
+    }
+
+    function calculateProgress() {
+        var progressWidth = 596.5;
+        const cx = currentXP;
+        const rx = requiredXP;
+
+        if (rx <= 0) return 1;
+        if (cx > rx) return progressWidth;
+
+        let width = (cx * 615) / rx;
+        if (width > progressWidth) width = progressWidth;
+        return width;
+    }
+
+    function shorten(text, len) {
+        if (typeof text !== 'string') return '';
+        if (text.length <= len) return text;
+        return text.substr(0, len).trim() + '...';
+    }
+
+    function toAbbrev(num) {
+        return abbrev(num);
+    }
 
     if (!avatar) throw new Error('Avatar is required');
     if (!username) throw new Error('Username is required');
-    if (!discriminator) throw new Error('Discriminator is required');
-    if (!currentXP) throw new Error('Current XP is required');
-    if (!requiredXP) throw new Error('Required XP is required');
-    if (!level) throw new Error('Level is required');
+    if (typeof discriminator !== 'number') throw new Error(`Discriminator must be a number, recieved "${typeof discriminator}"`);
+    if (typeof currentXP !== 'number') throw new Error(`Current XP must be a number, recieved "${typeof currentXP}"`);
+    if (typeof requiredXP !== 'number') throw new Error(`Required XP must be a number, recieved "${typeof requiredXP}"`);
+    if (typeof level !== 'number') throw new Error(`Level must be a number, recieved "${typeof level}"`);
 
-    if (!isNaN(discriminator)) throw new Error('Discriminator must be a number');
-    if (!isNaN(currentXP)) throw new Error('Current XP must be a number');
-    if (!isNaN(requiredXP)) throw new Error('Required XP must be a number');
-    if (!isNaN(level)) throw new Error('Level must be a number');
+    canvas = canvas.createCanvas(932, 282);
+    var ctx = canvas.getContext('2d');
 
+    // reset transparency
+    ctx.globalAlpha = 1;
+
+    // create background
+    ctx.fillStyle = this.data.background.image;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // add overlay
+    ctx.globalAlpha = this.data.overlay.level || 1;
+    ctx.fillStyle = this.data.overlay.color;
+    ctx.fillRect(20, 20, canvas.width - 40, canvas.height - 40);
+
+    // draw username
+    ctx.font = 'bold 36px Sans';
+    ctx.fillStyle = this.data.username.color;
+    ctx.textAlign = 'start';
+    var name = shorten(username, 10);
+
+    // apply username
+    ctx.fillText(`${name}`, 257 + 18.5, 164);
+
+    // draw discriminator
+    var discrim = `${discriminator}`;
+    if (discrim) {
+        ctx.font = '36px Sans';
+        ctx.fillStyle = this.data.discriminator.color;
+        ctx.textAlign = 'center';
+        ctx.fillText(`#${discrim.substr(0, 4)}`, ctx.measureText(name).width + 20 + 335, 164);
+    }
+
+    // fill level
+    ctx.font = `bold 36px Sans`;
+    ctx.fillStyle = this.data.level.color;
+    ctx.fillText(this.data.level.displayText, 800 - ctx.measureText(toAbbrev(parseInt(level))).width, 82);
+
+    ctx.font = `bold 32px Sans`;
+    ctx.fillStyle = '#e5f7b2';
+    ctx.textAlign = 'end';
+    ctx.fillText(toAbbrev(parseInt(level)), 860, 82);
+
+    // show progress
+    ctx.font = `bold 30px Sans`;
+    ctx.fillStyle = this.data.requiredXP.color;
+    ctx.textAlign = "start";
+    ctx.fillText("/ " + toAbbrev(requiredXP), 670 + ctx.measureText(toAbbrev(currentXP)).width + 15, 164);
     
+    ctx.fillStyle = this.data.currentXP.color;
+    ctx.fillText(toAbbrev(currentXP), 670, 164);
+
+    // draw progressbar
+    ctx.beginPath();
+    if (this.data.progressBar.rounded) {
+        // bg
+        ctx.fillStyle = this.data.progressBar.track.color;
+        ctx.arc(257 + 18.5, 147.5 + 18.5 + 36.25, 18.5, 1.5 * Math.PI, 0.5 * Math.PI, true);
+        ctx.fill();
+        ctx.fillRect(257 + 18.5, 147.5 + 36.25, 615 - 18.5, 37.5);
+        ctx.arc(257 + 615, 147.5 + 18.5 + 36.25, 18.75, 1.5 * Math.PI, 0.5 * Math.PI, false);
+        ctx.fill();
+
+        ctx.beginPath();
+        // apply color
+        if (this.data.progressBar.bar.type === "gradient") {
+            let gradientContext = ctx.createRadialGradient(calculateProgress(), 0, 500, 0);
+            this.data.progressBar.bar.color.forEach((color, index) => {
+                gradientContext.addColorStop(index, color);
+            });
+            ctx.fillStyle = gradientContext;
+        } else {
+            ctx.fillStyle = this.data.progressBar.bar.color;
+        }
+
+        // progress bar
+        ctx.arc(257 + 18.5, 147.5 + 18.5 + 36.25, 18.5, 1.5 * Math.PI, 0.5 * Math.PI, true);
+        ctx.fill();
+        ctx.fillRect(257 + 18.5, 147.5 + 36.25, calculateProgress(), 37.5);
+        ctx.arc(257 + 18.5 + calculateProgress(), 147.5 + 18.5 + 36.25, 18.75, 1.5 * Math.PI, 0.5 * Math.PI, false);
+        ctx.fill();
+    } else {
+
+        // progress bar
+        ctx.fillStyle = this.data.progressBar.bar.color;
+        ctx.fillRect(this.data.progressBar.x, this.data.progressBar.y, calculateProgress(), this.data.progressBar.height);
+
+        // outline
+        ctx.beginPath();
+        ctx.strokeStyle = this.data.progressBar.track.color;
+        ctx.lineWidth = 7;
+        ctx.strokeRect(this.data.progressBar.x, this.data.progressBar.y, this.data.progressBar.width, this.data.progressBar.height);
+    }
+
+    ctx.save();
+
+    // circle
+    ctx.beginPath();
+    ctx.arc(125 + 10, 125 + 20, 100, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
+
+    // draw avatar
+    ctx.drawImage(avatar, 35, 45, this.data.avatar.width + 20, this.data.avatar.height + 20);
+    ctx.restore();
+
+
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'rank.png');
+    message.channel.send(attachment);
 }
