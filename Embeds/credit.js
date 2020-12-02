8cb0c834c040350c257aa6291012902446677c8c
Tyler <tylerrp06@gmail.com>
2020-12-02 09:54:26 +0000

    Updated credit embed & recent database

--- Embeds/credit.js
+++ Embeds/credit.js
@@ -10,9 +10,9 @@ module.exports = {
     embed.setTitle('Billybobbeep | Credits')
     embed.setDescription('Thank you for using Billybobbeep!\n' +
       '\n' +
-      'Created by: **Spoink#2793**\n' +
-      'Scripted by: **Spoink#2793** & **Will Os#9857**\n' +
-      'Name development & suggestions: **wibbleywobbley♡#1564**\n\n' +
+      `Created by: **${client.users.cache.get('697194959119319130').tag}**\n` +
+      `Scripted by: **${client.users.cache.get('697194959119319130').tag}** & **${client.users.cache.get('303097521314725890').tag}**\n` +
+      `Name development & suggestions: **${client.users.cache.get('441613173003649028').tag}**\n\n` +
       'If you would like to view more, feel free to check out [our website](https://billybobbeep.tyler2p.repl.co/home)');
     embed.setColor(`${db.get(message.guild.id + '.embedColor') || '#447ba1'}`);
     embed.setFooter(`Requested by: ${message.author.tag}`);
