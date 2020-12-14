4dac26ca616e44bb93d429da96a909397f103ccd
Tyler <tylerrp06@gmail.com>
2020-12-14 19:16:35 +0000

    Bug fixes
    Bug fixes with serverstats.js

--- events/backend/serverstats.js
+++ events/backend/serverstats.js
@@ -2,23 +2,21 @@ const configFile = require('../../structure/config.json');
 const db = require('../../structure/global.js').db;
 
 module.exports = (member, client) => {
-  if (member.user.guild.id !== countChannel.serverID) return;
-
   let countChannel = {
-    total: db.get(member.user.guild.id + '.serverStats.totalNo'),
-    member: db.get(member.user.guild.id + '.serverStats.memberNo'),
-    bots: db.get(member.user.guild.id + '.serverStats.botNo'),
-    serverID: member.user.guild.id
+    total: db.get(member.guild.id + '.serverStats.totalNo'),
+    member: db.get(member.guild.id + '.serverStats.memberNo'),
+    bots: db.get(member.guild.id + '.serverStats.botNo'),
+    serverID: member.guild.id
   }
      
   if (!countChannel.total || !countChannel.member || !countChannel.bots) return;
-  var tu = db.set(member.user.guild.id + '.serverStats.totalNoText') || 'Total Users:'
-  var tm = db.set(member.user.guild.id + '.serverStats.memberNoText') || 'Members:';
-  var tb = db.get(member.user.guild.id + '.serverStats.botNoText') || 'Bots:';
+  var tu = db.set(member.guild.id + '.serverStats.totalNoText') || 'Total Users:'
+  var tm = db.set(member.guild.id + '.serverStats.memberNoText') || 'Members:';
+  var tb = db.get(member.guild.id + '.serverStats.botNoText') || 'Bots:';
   try {
-      client.channels.cache.get(countChannel.total).setName(`${tu} ${member.user.guild.memberCount}`);
-      client.channels.cache.get(countChannel.member).setName(`${tm} ${member.user.guild.members.cache.filter(m => !m.user.bot).size}`);
-      client.channels.cache.get(countChannel.bots).setName(`${tb} ${member.user.user.guild.members.cache.filter(m => m.user.bot).size}`);
+      client.channels.cache.get(countChannel.total).setName(`${tu} ${member.guild.memberCount}`);
+      client.channels.cache.get(countChannel.member).setName(`${tm} ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
+      client.channels.cache.get(countChannel.bots).setName(`${tb} ${member.guild.members.cache.filter(m => m.user.bot).size}`);
   } catch {
       return;
   }
