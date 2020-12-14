9087bdc7a5069b75fd52f82306f439c7c5b111a1
Tyler <tylerrp06@gmail.com>
2020-12-14 19:18:04 +0000

    Added permission checking

--- test.js
+++ test.js
@@ -8,3 +8,13 @@ db.fetchAll().forEach(data => {
     //console.log(data.data.level);
     console.log(data);
 });
+
+const Discord = require('discord.js');
+const chalk = require('chalk');
+const client = new Discord.Client();
+client.on('ready', () => {
+    (client.guilds.cache).array().forEach(guild => {
+        console.log(chalk.blue(guild.id) + ' - ' + chalk.green(guild.name) + ' - ' + chalk.red(guild.members.cache.get(client.user.id).hasPermission('ADMINISTRATOR')));
+    });
+});
+client.login(require('./structure/auth.js').token);
