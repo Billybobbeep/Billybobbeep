dc23dea15967c70f7f653a393065fbc7a693d419
Tyler <tylerrp06@gmail.com>
2020-12-27 18:23:16 +0000

    Added guildMember schema

--- events/client/database/models/guildMembers.js
+++ events/client/database/models/guildMembers.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const guildMemberSchema = new mongoose.Schema({
     memberId: String,
+    guildId: String
 }, { strict: false });
 
 module.exports = mongoose.model('guildMembers', guildMemberSchema);
