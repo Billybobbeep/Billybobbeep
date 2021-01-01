181132ffbbb23d5fa6c7a9618502ed7a92d4bb20
Tyler <tylerrp06@gmail.com>
2021-01-01 14:45:17 +0000

    See last commit

--- server.js
+++ server.js
@@ -17,61 +17,6 @@ module.exports = async (client) => {
       key = req.params.key;
       bot.data(function(client, data) { res.send(data) });
     });
-    app.get('/api/chatbot', async function(req, res) {
-      let message = req.query.message;
-      let debounce = false;
-
-      let reply = undefined;
-      if (!message) return res.json('missing message query');
-      if (message.toLowerCase() === 'hello' || message.toLowerCase() === 'hi' || message.toLowerCase() === 'hiya') reply = message.toLowerCase();
-      if (message.toLowerCase().includes('test')) reply = 'test command';
-      if (message.toLowerCase().includes('ily')) reply = 'ily2';
-      if (message.toLowerCase().startsWith('call') || message.toLowerCase().includes('emergency')) reply = 'I am not responsible for any form of emergency call.\nIf you need any service from the emergency services, please call them yourself or ask another guild member to do it for you.';
-      if (reply === undefined) {
-        let response = await fetch('https://some-random-api.ml/chatbot?message=' + message);
-        reply = await response.text();
-        if (reply.toLowerCase().startsWith('<!doctype html>')) res.json('This channel is causing issues for me. Please try again later.');
-        reply = reply.replace('response', '').replace('{', '').replace('}', '').replace('??', '?').replace('???', '?').replace('"', '').replace('"', '').replace('"', '').replace('error', '').replace(')', '').replace(':', '');
-      }
-      if (req.query.cap && req.query.cap === 'no' && req.query.punc && req.query.punc === 'no') {
-        res.json(reply.toLowerCase());
-      } else if (req.query.cap && req.query.cap === 'yes' && req.query.punc && req.query.punc === 'no') {
-        res.json(reply[0].toUpperCase() + reply.substring(1).toLowerCase());
-      } else if (req.query.cap && req.query.cap === 'yes' && req.query.punc && req.query.punc === 'yes') {
-        reply = reply[0].toUpperCase() + reply.substring(1).toLowerCase();
-        if (message.endsWith('?') || message.endsWith('!') || message.endsWith('.') || message.endsWith('/')) {
-          reply = reply;
-        } else {
-          reply = reply + '.'
-        }
-        res.json(reply.replace('?.', '?').replace('..', '.').replace('!.', '!'));
-      } else if (req.query.cap && req.query.cap === 'no' && req.query.punc && req.query.punc === 'yes') {
-        reply = reply.toLowerCase();
-        if (message.endsWith('?') || message.endsWith('!') || message.endsWith('.') || message.endsWith('/'))
-          reply = reply;
-        else
-          reply = reply + '.'
-        res.json(reply.replace('?.', '?').replace('..', '.').replace('!.', '!'));
-      } else if (req.query.cap && req.query.cap === 'full' && req.query.punc && req.query.punc === 'yes') {
-        reply = reply.toUpperCase();
-        if (message.endsWith('?') || message.endsWith('!') || message.endsWith('.') || message.endsWith('/')) {
-          reply = reply;
-        } else {
-          reply = reply + '.'
-        }
-        res.json(reply.replace('?.', '?').replace('..', '.').replace('!.', '!'));
-      } else if (req.query.cap && req.query.cap === 'full' && req.query.punc && req.query.punc === 'no') {
-        res.json(reply.toUpperCase());
-      } else {
-        reply = reply[0].toUpperCase() + reply.substring(1).toLowerCase();
-        if (message.endsWith('?') || message.endsWith('!') || message.endsWith('.') || message.endsWith('/')) {
-          reply = reply;
-        } else {
-          reply = reply + '.'
-        }
-        res.json(reply.replace('?.', '?').replace('..', '.').replace('!.', '!').replace('"', '').replace('"', ''));
-      }
-    });
     app.get('/', function(req, res) {
       res.redirect('/home');
     });
