b3761b8e250561150b637f1daf48f4aa067e1ac3
Tyler <tylerrp06@gmail.com>
2021-01-01 16:51:07 +0000

    Update timeOut.js

--- events/backend/timeOut.js
+++ events/backend/timeOut.js
@@ -46,15 +46,13 @@ function application(db, client) {
     const moment = require('moment');
     let appliedUsers = require('../client/database/models/awaiting');
     appliedUsers.find(function(err, data) {
-        let table = [];
         if (!data) return;
         data.forEach(result => {
-            table.push(result);
             result = result.replace('_', ' ').replace('_', ' ').replace('_', ' ').replace('_', ' ');
             result = result.split(/ +/g);
-            let user = client.users.cache.get(result[0]);
-            let date = result[1];
-            let job = result[2];
+            let user = client.users.cache.get(result.memberId);
+            let date = result.date;
+            let job = result.job;
             let emj = client.emojis.cache.get(require('../../structure/config.json').emoji)
             
             const embed = new MessageEmbed();
@@ -75,66 +73,58 @@ function application(db, client) {
             if (failed === true) {
                 embed.addField('Overall Score:', 'Failed');
                 embed.setFooter('Feel free to apply again in 2 minutes');
-                db.delete(user.id + '.jobs.awaiting');
-                db.set(user.id + '.jobs.lastApplied', Date.now());
+                db.findOneAndUpdate({ userId: user.id }, { job_awaiting: false, job_lastApplied: Date.now() });
             } else {
                 embed.addField('Overall Score:', 'Passed');
                 embed.setFooter('Feel free to start working when you\'re ready');
-                db.delete(user.id + '.jobs.awaiting');
-                db.set(user.id + '.jobs.lastApplied', Date.now());
-                db.set(user.id + `.jobs.job`, job.toLowerCase());
+                db.findOneAndUpdate({ userId: user.id }, { job_awaiting: false, job_lastApplied: Date.now(), job_name: job.toLowerCase() });
             }
             try {
                 user.send(embed);
             } catch {
-                return table.splice(table.indexOf(result.join('_')));
+                return;
             }
-            table.splice(table.indexOf(result.join('_')));
+            db.findOneAndRemove({ MemberId: result.memberId });
         });
-        db.set('awaiting', table);
     });
 }
 
 function mute(db, client) {
-    let MM = db.get('mutedMembers');
-    let guild;
-    let user;
-    let time;
+    let MM = require('../client/database/models/mutedMembers');
+    MM.find(function(err, data) {
+        let guild;
+        let user;
+        let time;
 
-    if (!MM) return;
-    if (MM.length < 1) return;
+        if (!data) return;
 
-    MM.forEach(result => {
-        result = result.replace('_', ' ').replace('_', ' ');
-        result = result.split(/ +/g);
-        guild = result[0];
-        user =  result[1];
-        time = result[2];
-        
-        guild = client.guilds.cache.get(guild);
-        let member = guild.members.cache.get(user);
-        if (!member) return remove(MM, db, guild, user, time, client);
-        if (member.roles.cache.find(role => role.id === db.get(guild.id + '.mutedRole'))) {
-            if (Date.now() > time) {
-                remove(MM, db, guild, user, time, client, 'mute');
-            }
-        } else {
-            remove(MM, db, guild, user, time, client, 'mute');
-        }
+        data.forEach(result => {
+            guild = result.guildId;
+            user =  result.memberId;
+            time = result.time;
+            
+            guild = client.guilds.cache.get(guild);
+            let member = guild.members.cache.get(user);
+            if (!member) return remove(db, guild, user);
+            if (member.roles.cache.find(role => role.id === db.get(guild.id + '.mutedRole'))) {
+                if (Date.now() > time)
+                remove(db, guild, user, 'mute');
+            } else
+                remove(db, guild, user, 'mute');
+        });
     });
 }
 
-function remove(table, db, guild, user, time, client, string) {
+function remove(db, guild, user, string) {
+    const guildData = require('../client/database/models/guilds');
     let member = guild.members.cache.get(user);
-    table.splice(table.indexOf(guild.id.toString() + '_' + user.toString() + '_' + time.toString()), 1);
-    db.set('mutedMembers', table);
+    let mutedRole = guildData.findOne({ guildId: guild.id }).then(result => result.mutedRole);
+    db.findOneAndRemove({ memberId: user.id });
     if (string === 'mute') {
         setTimeout(() => {
-            let mutedRole = db.get(guild.id +'.mutedRole');
             let role = guild.roles.cache.find(role => role.id === mutedRole);
-            if (member.roles.cache.find(role => role.id === mutedRole)) {
+            if (member.roles.cache.find(role => role.id === mutedRole))
                 member.roles.remove(role.id);
-            }
         }, 50);
     }
 }
