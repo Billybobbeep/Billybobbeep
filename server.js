f3ad89fbe85cb5623bc0c8a17cddfc8b5a8d3639
Tyler <tylerrp06@gmail.com>
2021-01-01 16:51:49 +0000

    Update server.js
    Updated 404 error pages, api's and removed useless functions

--- server.js
+++ server.js
@@ -6,46 +6,40 @@ module.exports = async (client) => {
   const bot = require('./utils/data.js');
   const fetch = require('node-fetch');
   const logging = require('./utils/functions').logging;
-  const guildData = require('./events/client/database/models/guilds');
 
   app.use('/style', express.static('style'));
   app.set('view engine', 'ejs');
 
-  function listen() {
-    app.post('/api/database/:name/get', (req, res) => {
-      if (req.params.name !== 'all') res.json('Error: Invalid database');
-      key = req.params.key;
-      bot.data(function(client, data) { res.send(data) });
-    });
-    app.get('/', function(req, res) {
-      res.redirect('/home');
-    });
-    app.get('/home', function(req, res) {
-      res.sendFile(__dirname + '/Public/index.html');
-    });
-    app.get('/home/status', function(req, res) {
-      res.redirect('/home/analytics');
-    });
-    app.get('/home/analytics', (req, res) => {
-      res.sendFile(__dirname + '/Public/analytics.html')
-    });
-    app.get('/discord/invite', function(req, res) {
-      res.redirect('https://discord.com/invite/qNJEj3s');
-    });
+  app.post('/api/database/:name/get', (req, res) => {
+    if (req.params.name !== 'all') res.json('Error: Invalid database');
+    key = req.params.key;
+    bot.data(function(client, data) { res.send(data) });
+  });
+  app.get('/', function(req, res) {
+    res.redirect('/home');
+  });
+  app.get('/home', function(req, res) {
+    res.sendFile(__dirname + '/Public/index.html');
+  });
+  app.get('/home/status', function(req, res) {
+    res.redirect('/home/analytics');
+  });
+  app.get('/home/analytics', (req, res) => {
+    res.sendFile(__dirname + '/Public/analytics.html')
+  });
+  app.get('/discord/invite', function(req, res) {
+    res.redirect('https://discord.com/invite/qNJEj3s');
+  });
 
-    app.use(function(req, res) {
-      if (req.url.toLowerCase().startsWith('/api')) {
-        res.status(404);
-        res.json('An invalid API key was sent.')
-      } else {
-        res.status(404);
-        res.sendFile(__dirname + '/Public/error404.html');
-      }
-    });
+  app.use(function(req, res) {
+    if (req.url.toLowerCase().startsWith('/api')) {
+      res.status(404);
+      res.json({ error: 'Cannot find what you\'re looking for'});
+    } else {
+      res.status(404);
+      res.sendFile(__dirname + '/Public/error404.html');
+    }
+  });
 
-    app.listen(port, () => { console.log('Billybobbeep is online') });
-
-  }
-
-  listen();
+  app.listen(port, () => console.log('Billybobbeep is online'));
 }
