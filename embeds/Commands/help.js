4b9064399bf3c8847df654ed22fa25e2aed31eb4
Tyler <tylerrp06@gmail.com>
2021-01-26 17:51:51 +0000

    Update help.js

--- embeds/Commands/help.js
+++ embeds/Commands/help.js
@@ -9,7 +9,6 @@ module.exports = async(msg, args, prefix, message) => {
         .addField(`${prefix}help `, 'Shows a quick briefing', false)
         .addField(`${prefix}info`, 'Gives more details about the bot', false)
         .addField(`${prefix}serverinfo`, 'Provides you with information on the server', false)
-        .addField(`${prefix}credits`, 'Provides you with Billybobbeep\'s development credits', false)
         .addField(`${prefix}setup`, 'Helps you customise billy for your server', false)
         .setFooter(`Requested by: ${message.author.tag}`)
         .setTimestamp()
