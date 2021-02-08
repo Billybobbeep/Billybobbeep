776d1e056d98c53b09b2500bda9a1b3861ca55e8
Tyler <tylerrp06@gmail.com>
2021-02-08 15:30:47 +0000

    Updated activities

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,12 +1,12 @@
 module.exports = (client) => {
-    let activities = [`~help`, `Version ${require('../../package.json').version}`];
-    i = 0;
-    setInterval(() => {
-      client.user.setActivity(`${activities[i++ % activities.length]}`, {type: 'LISTENING'});
-    }, 10000);
-    console.log(`Total Guilds: ${client.guilds.cache.size}\nTotal Members: ${client.users.cache.size}`)
-    require('../backend/timeOut.js')(client);
-    require('./database/connection.js')();
+  let activities = [`~help`, `Version ${(require('../../package.json').version)[0]}${(require('../../package.json').version)[1]}${(require('../../package.json').version)[2]}`];
+  i = 0;
+  setInterval(() => {
+    client.user.setActivity(`${activities[i++ % activities.length]}`, {type: 'LISTENING'});
+  }, 10000);
+  console.log(`Total Guilds: ${client.guilds.cache.size}\nTotal Members: ${client.users.cache.size}`)
+  require('../backend/timeOut.js')(client);
+  require('./database/connection.js')();
 }
 
 module.exports.reconnecting = (client) => {
