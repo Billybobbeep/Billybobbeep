7468ecd98cf89acb468c99233dd607d11cc434fb
Tyler <tylerrp06@gmail.com>
2021-02-10 20:55:40 +0000

    Bug fixes

--- structure/auth.js
+++ structure/auth.js
@@ -1,6 +1,3 @@
 module.exports = {
-    token: 'NzMxNDk4ODQyODEzMzY2MzA0.Xwm7Yg.OJluJAtnoTSAMRdJ1ior8QaG7O0',
-    api_key: 'cCImxEaGXIFLuaVU',
-    api_bid: '154297',
-    mongoDb: 'mongodb+srv://Tyler2P:thetrollking@billybobbeep.c05sy.mongodb.net/billybobbeep?retryWrites=true&w=majority'
+    token: 'your-token-here'
 }
