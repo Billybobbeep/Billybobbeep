28f1742d4562a4f0a50257b06f4d1903a75dfc42
Tyler Pearson <tylerrp06@gmail.com>
2021-02-22 12:46:08 +0000

    Updated commands

---
+++ commands/info/botinfo.js
@@ -0,0 +1,38 @@
+module.exports = {
+    name: 'info',
+    description: 'View billybobbeep\'s info',
+    guildOnly: true,
+    execute(message, prefix, client) {
+      const { MessageEmbed, version: djsversion } = require('discord.js');
+      const { version } = require('../package.json');
+      const { utc } = require('moment');
+      const guildData = require('../events/client/database/models/guilds');
+      const os = require('os');
+      const ms = require('ms');
+  
+      const core = os.cpus()[0];
+      guildData.findOne({ guildId: message.guild.id }).then(result => {
+        const embed = new MessageEmbed()
+          .setDescription("**Billybobbeep | Bot Info**")
+          .setThumbnail(client.user.displayAvatarURL())
+          .setColor(result.embedColor)
+          .setFooter(`Requested by: ${message.author.tag}`)
+          .addField('General', [
+            `**Name:** ${client.user.tag} (${client.user.id})`,
+            `**Servers:** ${client.guilds.cache.size.toLocaleString()} `,
+            `**Users:** ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}`,
+            `**Creation Date:** ${utc(client.user.createdTimestamp).format('Do MMMM YYYY HH:mm:ss')}`,
+            `**Node.js:** ${process.version}`,
+            `**Version:** v${version}`,
+            `**Discord.js:** v${djsversion}`,
+            '\u200b'
+          ])
+          .addField('System', [
+            `**✰ Platform:** ${process.platform}`,
+            `**✰ Uptime:** ${ms(os.uptime() * 1000, { long: true })}`,
+          ])
+          .setTimestamp();
+        message.channel.send(embed);
+      });
+    }
+  }
--- commands/info/invite.js
+++ commands/info/invite.js
@@ -6,7 +6,8 @@ module.exports = {
     const Discord = require('discord.js');
     const embed = Discord.MessageEmbed();
     embed.addField('Invite the bot to your server', '[https://discord.com/oauth2/authorize](https://discord.com/oauth2/authorize?client_id=731498842813366304&permissions=8&scope=bot)');
-    embed.addField('View more information', `[View ${client.user.username} on top.gg](https://top.gg/bot/731498842813366304)`)
-    embed.addField(`Join the official ${client.user.username} server`, '[Billybobbeep Help Center](AUGX9sywnP)')
+    embed.addField('View more information', `[View ${client.user.username} on top.gg](https://top.gg/bot/731498842813366304)`);
+    embed.addField(`Join the official ${client.user.username} server`, '[Billybobbeep Help Center](AUGX9sywnP)');
+    message.channel.send(embed);
   }
 }
