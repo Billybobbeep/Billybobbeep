3924f64940e7a44b5eb39b2e3926eafa0a6af8b9
Tyler Pearson <tylerrp06@gmail.com>
2021-02-22 03:08:40 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 module.exports = async (client) => {
   const express = require('express');
   const app = express();
-  const Discord = require('discord.js')
-  let port = 3000;
   const bot = require('./utils/data.js');
-  const fetch = require('node-fetch');
-  const logging = require('./utils/functions').logging;
+  const guildData = require('./events/client/database/models/guilds');
+  const port = 3000;
+  
 
   app.use('/style', express.static('style'));
   app.set('view engine', 'ejs');
@@ -20,21 +19,69 @@ module.exports = async (client) => {
     res.sendFile(__dirname + '/public/analytics.html')
   });
   app.get('/discord/invite', function(req, res) {
-    res.redirect('https://discord.com/invite/qNJEj3s');
+    res.redirect('https://discord.com/oauth2/authorize?client_id=731498842813366304&permissions=8&scope=bot');
   });
 
-  app.post('/api/:name', (req, res) => {
+  app.post('/images/guilds/:id', function(req, res) {
+    if (client.guilds.cache.get(req.params.id)) {
+      res.json({ data: { icon: client.guilds.cache.get(req.params.id).iconURL({ dynamic: true }) }, error: null });
+    } else {
+      res.status(404);
+      res.json({ error: 'Guild not found', data: null });
+    }
+  });
+  app.post('/api/credits', function(req, res) {
+    if (client.users.cache.get('303097521314725890') && client.users.cache.get('697194959119319130') && client.users.cache.get('441613173003649028')) {
+      res.json({ data: [client.users.cache.get('303097521314725890').tag, client.users.cache.get('697194959119319130').tag, client.users.cache.get('441613173003649028').tag], error: null });
+    } else if (client.users.cache.get('303097521314725890') && client.users.cache.get('697194959119319130')) {
+      res.json({ data: [client.users.cache.get('303097521314725890').tag, client.users.cache.get('697194959119319130').tag, 'wibbleywobbley#1564'], error: null });
+    } else if (client.users.cache.get('303097521314725890') && client.users.cache.get('441613173003649028')) {
+      res.json({ data: [client.users.cache.get('303097521314725890').tag, 'Spoink#2793', client.users.cache.get('441613173003649028').tag], error: null });
+    } else if (client.users.cache.get('697194959119319130') && client.users.cache.get('441613173003649028')) {
+      res.json({ data: ['Will Os#9857', client.users.cache.get('697194959119319130').tag, client.users.cache.get('441613173003649028').tag], error: null });
+    } else {
+      res.json({ data: ['Will Os#9857', 'Spoink#2793', 'wibbleywobbley#1564'], error: null });
+    }
+  });
+  app.post('/api/guilds/info', function(req, res) {
+    if (!req.query.guildId) return res.json({ error: 'Could not find guild ID query', data: null });
+    if (!client.guilds.cache.get(req.query.guildId)) {
+      res.json({ error: 'Guild not in selected cache', data: null });
+    } else {
+      let guild = client.guilds.cache.get(req.query.guildId);
+      try {
+        res.json({ data: { name: guild.name, members: guild.memberCount, region: guild.region, available: guild.available }, error: null });
+      } catch {
+        res.json({ error: 'Internal error', data: null});
+      }
+    }
+  });
+  app.get('/api/guilds/data', function(req, res) {
+    if (!req.query.guildId) return res.json({ error: 'Could not find guild ID query', data: null });
+    if (!client.guilds.cache.get(req.query.guildId)) {
+      res.json({ error: 'Guild not in selected cache', data: null });
+    } else {
+      guildData.findOne({ guildId: req.query.guildId }).then(result => {
+        if (!result) return res.json({ error: 'Guild could not be found', data: null });
+        res.json({ data: result });
+      }).catch(() => {
+        res.json({ error: 'Internal error', data: null })
+      });
+    }
+  });
+  app.post('/api/:name', function(req, res) {
     if (req.params.name === 'all') {
       bot.data(function(client, data) { res.send(data) });
     } else {
       res.status(404);
-      res.json('Error: Invalid database');
+      res.json({ error: 'Invalid database', data: null });
     }
   });
+
   app.use(function(req, res) {
     if (req.url.toLowerCase().startsWith('/api')) {
       res.status(404);
-      res.json({ error: 'Cannot find what you\'re looking for'});
+      res.json({ error: 'Cannot find what you\'re looking for', data: null });
     } else {
       res.status(404);
       res.sendFile(__dirname + '/public/error404.html');
