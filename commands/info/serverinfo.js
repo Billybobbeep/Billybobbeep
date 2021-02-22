4c05d31eaaa6a194a3f233136d10655ef54f9237
Tyler Pearson <tylerrp06@gmail.com>
2021-02-22 09:45:30 +0000

    Updated serverInfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -51,34 +51,23 @@ module.exports = {
 				.setThumbnail(message.guild.iconURL({ dynamic: true }))
 				.setFooter(`Requested by: ${message.author.tag}`)
 				.addField('General', [
-					`**✰ Name:** ${message.guild.name}`,
-					`**✰ ID:** ${message.guild.id}`,
-					`**✰ Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
-					`**✰ Region:** ${regions[message.guild.region]}`,
-					`**✰ Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
-					`**✰ Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
-					`**✰ Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
-					`**✰ Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`,
+					`**Name:** ${message.guild.name}`,
+					`**ID:** ${message.guild.id}`,
+					`**Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
+					`**Region:** ${regions[message.guild.region]}`,
+					`**Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
+					`**Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
+					`**Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
+					`**Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')}, ${moment(message.guild.createdTimestamp).fromNow()}`,
 					'\u200b'
 				])
 				.addField('Statistics', [
-					`**✰ Role Count:** ${roles.length}`,
-					`**✰ Emoji Count:** ${emojis.size}`,
-					`**✰ Regular Emoji Count:** ${emojis.filter(emoji => !emoji.animated).size}`,
-					`**✰ Animated Emoji Count:** ${emojis.filter(emoji => emoji.animated).size}`,
-					`**✰ Member Count:** ${message.guild.memberCount}`,
-					`**✰ Members:** ${members.filter(member => !member.user.bot).size}`,
-					`**✰ Bots:** ${members.filter(member => member.user.bot).size}`,
-					`**✰ Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
-					`**✰ Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
-					`**✰ Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
-					'\u200b'
-				])
-				.addField('Members', [
-					`**✰ Online:** ${members.filter(member => member.presence.status === 'online').size}`,
-					`**✰ Idle:** ${members.filter(member => member.presence.status === 'idle').size}`,
-					`**✰ Do Not Disturb:** ${members.filter(member => member.presence.status === 'dnd').size}`,
-					`**✰ Offline:** ${members.filter(member => member.presence.status === 'offline').size}`,
+					`**Role Count:** ${roles.length}`,
+					`**Emoji Count:** ${emojis.size}`,
+					`**Members:** ${message.guild.memberCount}`,
+					`**Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
+					`**Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
+					`**Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
 					'\u200b'
 				])
 				.setTimestamp();
