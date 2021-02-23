3a0c375fc03cbb3e99f1a0e8a43dc00e2be83160
Tyler <tylerrp06@gmail.com>
2021-02-23 17:12:36 +0000

    Removed useless files & reformatted folders

--- structure/auth.js
+++ structure/auth.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'client-token',
-    secret: 'client-secret',
-    mongoDb: 'mongoDB-connection-key'
+    token: 'NzMxNDk4ODQyODEzMzY2MzA0.Xwm7Yg.MOz6EMTKpMSJXI66CUkqHOUcvoU',
+    secret: 'h_Zr5wSYGDSPbNRjmGmkn8lR8FhZJOFC',
+    mongoDb: 'mongodb+srv://Tyler2P:MV9agShyQaTHotKr@billybobbeep.c05sy.mongodb.net/billybobbeep?retryWrites=true&w=majority'
 }
