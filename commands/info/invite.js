71a9b6679a92296ffe2a8014a87c47edf96c34f9
Tyler <tylerrp06@gmail.com>
2021-02-24 19:35:18 +0000

    Bug fixes
    Fixed bugs with invite command

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -4,7 +4,7 @@ module.exports = {
 	catagory: 'info',
 	execute (message, prefix, client) {
 		const Discord = require('discord.js');
-		const embed = Discord.MessageEmbed();
+		const embed = new Discord.MessageEmbed();
 		embed.addField('Invite the bot to your server', `[https://discord.com/oauth2/authorize](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`);
 		embed.addField('View more information', `[View ${client.user.username} on top.gg](https://top.gg/bot/${client.user.id})`);
 		embed.addField(`Join the official ${client.user.username} server`, '[Billybobbeep Help Center](AUGX9sywnP)');
