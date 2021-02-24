396bc91e79e8c04d5656731be7c60656d4cb1042
Tyler <tylerrp06@gmail.com>
2021-02-24 19:26:44 +0000

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,21 +1,9 @@
 module.exports = (client) => {
-  let activities = [`~help`, `Version ${(require('../../package.json').version)[0]}${(require('../../package.json').version)[1]}${(require('../../package.json').version)[2]}`];
+  let activities = [`~help`, `to v${(require('../../package.json').version)[0]}${(require('../../package.json').version)[1]}${(require('../../package.json').version)[2]}`];
   i = 0;
   setInterval(() => {
     client.user.setActivity(`${activities[i++ % activities.length]}`, {type: 'LISTENING'});
   }, 10000);
   console.log(`Total Guilds: ${client.guilds.cache.size}\nTotal Members: ${client.users.cache.size}`)
   require('../backend/timeOut.js')(client);
-  require('./database/connection.js')();
-}
-
-module.exports.reconnecting = (client) => {
-  let guild = client.guilds.cache.get('733442092667502613');
-  let channel = guild.channels.cache.get('733595548577300542');
-  let user = guild.members.cache.get(require('../../utils/config.json').SpoinkID);
-  try {
-    user.send(`${client.user.username} cannot connect`);
-  } catch {
-    channel.send(`<@!${user.id}> Please turn your DMs on`);
-  }
 }
