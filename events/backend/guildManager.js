173d33949f9d92372fbe72e8db230d5359381ae8
Tyler <tylerrp06@gmail.com>
2021-02-24 18:28:35 +0000

    Update guildManager.js
    Bug fixes

--- events/backend/guildManager.js
+++ events/backend/guildManager.js
@@ -5,7 +5,8 @@ const embed2 = new MessageEmbed();
 const logging = require('../../utils/functions.js').logging;
 
 module.exports.add = async (guild, client) => {
-      setTimeout(async function() {
+    if (!guild.name) return;
+    setTimeout(async function() {
         let channelID;
         let channels = guild.channels.cache;
 
@@ -19,73 +20,75 @@ module.exports.add = async (guild, client) => {
         }
 
         guildData.findOne({ guildId: guild.id }).then(async result => {
-          let channel = await guild.channels.cache.get(guild.systemChannelID || channelID);
-          embed.setTitle('Billybobbeep | Welcome');
-          embed.setColor('#447ba1');
-          embed.setTimestamp();
-          embed.setDescription(`Thank you for adding me to your server.\n\nThe default prefix is \`~\`, You can change the prefix with the command \`~prefix\`\n\nTo view the commands view \`~cmds\` and to customise the bot for your server feel free to check out \`~setup\``);
-          setTimeout(() => {
-            try {
-              channel.send(embed);
-            } catch {
-              console.log('Could not send welcome embed in ' + guild.name);
-            }
-          }, 300);
-        }, 10000);
+            let channel = await guild.channels.cache.get(guild.systemChannelID || channelID);
+            embed.setTitle('Billybobbeep | Welcome');
+            embed.setColor('#447ba1');
+            embed.setTimestamp();
+            embed.setDescription(`Thank you for adding me to your server.\n\nThe default prefix is \`~\`, You can change the prefix with the command \`~prefix\`\n\nTo view the commands view \`~cmds\` and to customise the bot for your server feel free to check out \`~setup\``);
+            setTimeout(() => {
+                try {
+                    channel.send(embed);
+                } catch {
+                    console.log('Could not send welcome embed in ' + guild.name);
+                }
+            }, 300);
+        });
 
         let role;
-          guild.roles.create({
+        guild.roles.create({
             data: {
                 name: 'Billy 🤩',
                 color: '#e5f7b2',
                 permissions: 8
             }
-          }).then(role => {
+        }).then(role => {
             guild.member(client.user).roles.add(role);
             role.setHoist(true);
             const highestRole = guild.me.roles.highest;
             role.setPosition(highestRole.position - 1);
-          }).catch(() => { return });
+        }).catch(() => { return });
 
-
-      embed2.setTitle('Guild Added')
-      embed2.setDescription(
+        embed2.setTitle('Guild Added');
+        embed2.setDescription(
         `**Guild Name:** ${guild.name}\n` +
-        `**Guild ID:** ${guild.id}`)
-      embed2.setColor('#447ba1')
-      embed2.setTimestamp()
-      embed2.setThumbnail(guild.iconURL({ dynamic: true }));
-      embed2.setFooter(`Total Guilds: ${client.guilds.cache.size}`);
-      logging(embed2, '733442092667502613', client, 'guild');
+        `**Guild ID:** ${guild.id}`);
+        embed2.setColor('#447ba1');
+        embed2.setTimestamp();
+        embed2.setThumbnail(guild.iconURL({ dynamic: true }));
+        embed2.setFooter(`Total Guilds: ${client.guilds.cache.size}`);
+        logging(embed2, '733442092667502613', client, 'guild');
 
-      const newData = new guildData({ guildId: guild.id, embedColor: '#447ba1' });
-      newData.save();
-    });
-  }
+        const newData = new guildData({ guildId: guild.id, embedColor: '#447ba1' });
+        newData.save();
+    }, 1000);
+}
 
 module.exports.remove = (guild, client) => {
+    if (!guild.name) return;
+    const embed = new MessageEmbed()
+        .setTitle('Guild Removed')
+        .setDescription(
+        `**Guild Name:** ${guild.name}\n` +
+        `**Guild ID:** ${guild.id}`)
+        .setColor('#447ba1')
+        .setTimestamp()
+        .setThumbnail(guild.iconURL({ dynamic: true }))
+        .setFooter(`Total Guilds: ${client.guilds.cache.size}`);
+    logging(embed, '733442092667502613', client, 'guild');
 
-  const embed = new MessageEmbed()
-    .setTitle('Guild Removed')
-    .setDescription(
-      `**Guild Name:** ${guild.name}\n` +
-      `**Guild ID:** ${guild.id}`)
-    .setColor('#447ba1')
-    .setTimestamp()
-    .setThumbnail(guild.iconURL({ dynamic: true }))
-    .setFooter(`Total Guilds: ${client.guilds.cache.size}`);
-  logging(embed, '733442092667502613', client, 'guild');
-
-  const guildData = require('../client/database/models/guilds');
-  guildData.findOne({ guildId: guild.id }).then(res => res.delete());
-  const guildMemberData = require('../client/database/models/guildMembers');
-  guildMemberData.find(function(err, result) {
-    if (err) return console.log(err);
-    if (!result) return;
-    result.forEach(data => {
-      if (data.guildId && data.guildId === guild.id) {
-        data.delete();
-      }
+    const guildData = require('../client/database/models/guilds');
+    guildData.findOne({ guildId: guild.id }).then(res => {
+        if (!res) return;
+        res.delete();
     });
-  });
+    const guildMemberData = require('../client/database/models/guildMembers');
+    guildMemberData.find(function(err, result) {
+        if (err) return console.log(err);
+        if (!result) return;
+        result.forEach(data => {
+            if (data.guildId && data.guildId === guild.id) {
+                data.delete();
+            }
+        });
+    });
 }
