bd8dab9190961b924bd7df6b61e5360bd4c8381f
Tyler <tylerrp06@gmail.com>
2021-02-24 19:26:13 +0000

    Updated misc commands

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -1,13 +1,13 @@
 module.exports = {
-  name: 'invite',
-  description: 'Generate a bot invite',
-  catagory: 'info',
-  execute (message, prefix, client) {
-    const Discord = require('discord.js');
-    const embed = Discord.MessageEmbed();
-    embed.addField('Invite the bot to your server', '[https://discord.com/oauth2/authorize](https://discord.com/oauth2/authorize?client_id=731498842813366304&permissions=8&scope=bot)');
-    embed.addField('View more information', `[View ${client.user.username} on top.gg](https://top.gg/bot/731498842813366304)`);
-    embed.addField(`Join the official ${client.user.username} server`, '[Billybobbeep Help Center](AUGX9sywnP)');
-    message.channel.send(embed);
-  }
+	name: 'invite',
+	description: 'Generate a bot invite',
+	catagory: 'info',
+	execute (message, prefix, client) {
+		const Discord = require('discord.js');
+		const embed = Discord.MessageEmbed();
+		embed.addField('Invite the bot to your server', `[https://discord.com/oauth2/authorize](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`);
+		embed.addField('View more information', `[View ${client.user.username} on top.gg](https://top.gg/bot/${client.user.id})`);
+		embed.addField(`Join the official ${client.user.username} server`, '[Billybobbeep Help Center](AUGX9sywnP)');
+		message.channel.send(embed);
+	}
 }
--- commands/misc/vote.js
+++ commands/misc/vote.js
@@ -9,7 +9,7 @@ module.exports = {
 
         guildData.findOne({ guildId: message.guild.id }).then(result => {
             embed.setTitle('Vote for Billybobbeep');
-            embed.setDescription('Please [see here](https://top.gg/bot/731498842813366304/vote) to vote for Billybobbeep on [top.gg](https://top.gg)');
+            embed.setDescription(`Please [see here](https://top.gg/bot/${client.user.id}/vote) to vote for Billybobbeep on [top.gg](https://top.gg)`);
             embed.setColor(result.embedColor);
             message.channel.send(embed);
         });
