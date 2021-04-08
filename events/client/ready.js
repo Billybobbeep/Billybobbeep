8822ca475b4fca16009a7ff2e26c28d70e1ea177
Tyler <tylerrp06@gmail.com>
2021-04-08 18:51:27 +0100

    Improved formatting

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,9 +1,9 @@
 module.exports = (client) => {
   let activities = [`~help`, `to v${(require('../../package.json').version)[0]}${(require('../../package.json').version)[1]}${(require('../../package.json').version)[2]}`];
-  i = 0;
+  let i = 0;
   setInterval(() => {
     client.user.setActivity(`${activities[i++ % activities.length]}`, {type: 'LISTENING'});
   }, 10000);
-  console.log(`Total Guilds: ${client.guilds.cache.size}\nTotal Members: ${client.users.cache.size}`)
+  console.log(`Total Guilds: ${client.guilds.cache.size}\nTotal Members: ${client.users.cache.size}`);
   require('../backend/timeOut.js')(client);
 }
