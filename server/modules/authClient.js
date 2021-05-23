c1393bd15426dcd543566a34ea9bf2f3d7e56f06
Tyler <tylerrp06@gmail.com>
2021-05-23 20:30:46 +0100

    Removed unneeded files
    Uncluttered files

--- server/modules/authClient.js
+++
@@ -1,6 +0,0 @@
-const discordAuth = require('disco-oauth');
-const client = new discordAuth(process.env.clientId, process.env.clientSecret);
-client.setRedirect('https://billybobbeep-1.tyler2p.repl.co/auth');
-client.setScopes(['identify', 'guilds']);
-
-module.exports = client;
