c187ce7542e2e3d8798774e645fd5812ffae77d8
Tyler <tylerrp06@gmail.com>
2021-05-26 18:21:37 +0100

    Added user voting rewards
    Now supports rewards for voting

--- server/api.js
+++ server/api.js
@@ -1,11 +1,53 @@
 const guildData = require('../events/client/database/models/guilds');
 const express = require('express');
 const router = express.Router();
-const { Client } = require('discord.js');
+const { Client, MessageEmbed } = require('discord.js');
 const client = new Client();
 client.login(process.env.token);
 
-router.get('/api/images/guilds/:id', function(req, res) {
+let cache = {
+    lastVote: false,
+    lastVoteDate: false
+}
+
+async function userVoted(req, res) {
+    let user = false;
+    user = await client.users.fetch(req.body.user).catch(() => res.json({ error: 'Could not find user' }));
+    if (cache.lastVotedDate && cache.lastVoteDate < new Date().getTime() + 60000) return;
+    if (user && (req.body.bot).toString() == (client.user.id).toString() && (req.body.type).toLowerCase() == 'upvote') {
+        if (!user) return res.json({ error: 'Could not find user' });
+        const userData = require('../events/client/database/models/users');
+        userData.findOne({ userId: req.body.user }).then(async result => {
+            if (!result) return;
+            const embed = new MessageEmbed();
+            embed.setTitle('Thank you for voting!');
+            embed.setDescription('You have recieved `$100` for voting on [top.gg](https://top.gg/bot/' + client.user.id + ')');
+            embed.setColor('#447ba1');
+            await user.send(embed).catch(err => res.json({ error: 'User has DMs turned off' }));
+            result.economy_balance = result.economy_balance + 100;
+            result.save();
+            cache.lastVoteDate = new Date().getTime();
+            res.json({ response: 200 });
+            let votersChannel = await client.channels.fetch('847150442558390282');
+            if (votersChannel) votersChannel.send(`${user.username} has ${req.body.type}d`);
+        });
+    } else {
+        res.json({ error: 'Internal Error' });
+    }
+}
+
+router.post('/api/user/voted', function (req, res) {
+    if (typeof cache.lastVote == 'string' && cache.lastVote == req.body.user) return;
+    let user = client.users.fetch(req.body.user);
+    if (!user) {
+        client.once('ready', () => {
+            userVoted(req, res);
+        });
+    } else
+        userVoted(req, res);
+});
+
+router.get('/api/images/guilds/:id', function (req, res) {
     if (client.guilds.cache.get(req.params.id)) {
         //res.json({ data: { icon: client.guilds.cache.get(req.params.id).iconURL({ dynamic: true }) }, error: null });
         try {
@@ -19,7 +61,7 @@ router.get('/api/images/guilds/:id', function(req, res) {
     }
 });
 
-router.post('/api/credits', function(req, res) {
+router.post('/api/credits', function (req, res) {
     if (client.users.cache.get('303097521314725890') && client.users.cache.get('697194959119319130') && client.users.cache.get('441613173003649028')) {
         res.json({ data: [client.users.cache.get('303097521314725890').tag, client.users.cache.get('697194959119319130').tag, client.users.cache.get('441613173003649028').tag], error: null });
     } else if (client.users.cache.get('303097521314725890') && client.users.cache.get('697194959119319130')) {
@@ -33,7 +75,7 @@ router.post('/api/credits', function(req, res) {
     }
 });
 
-router.post('/api/guilds/info', function(req, res) {
+router.post('/api/guilds/info', function (req, res) {
     if (!req.query.guildId) return res.json({ error: 'Could not find guild ID query', data: null });
     if (!client.guilds.cache.get(req.query.guildId)) {
         res.json({ error: 'Guild not in selected cache', data: null });
@@ -42,12 +84,12 @@ router.post('/api/guilds/info', function(req, res) {
         try {
             res.json({ data: { name: guild.name, members: guild.memberCount, region: guild.region, available: guild.available }, error: null });
         } catch {
-            res.json({ error: 'Internal error', data: null});
+            res.json({ error: 'Internal error', data: null });
         }
     }
 });
 
-router.get('/api/guilds/data', function(req, res) {
+router.get('/api/guilds/data', function (req, res) {
     if (!req.query.guildId) return res.json({ error: 'Could not find guild ID query', data: null });
     if (!client.guilds.cache.get(req.query.guildId)) {
         res.json({ error: 'Guild not in selected cache', data: null });
