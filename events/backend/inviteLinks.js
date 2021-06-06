31a55b1c923ecd54938f86a776bf72a5c5370628
Tyler <tylerrp06@gmail.com>
2021-06-06 19:14:15 +0100

    Updated invite link detecting
    Big fixes

--- events/backend/inviteLinks.js
+++ events/backend/inviteLinks.js
@@ -1,40 +1,53 @@
 const guildData = require('../client/database/models/guilds');
 const Discord = require('discord.js');
+const axios = require('axios');
+let il = ['dsc.gg', 'discord.gg', 'discord.com/invite'];
 
-module.exports = async (message, client) => {
-  if (!message.guild) return;
-  guildData.findOne({ guildId: message.guild.id }).then(async result => {
-    let prefix = result.prefix || '~'
-    const embed = new Discord.MessageEmbed()
-    const logging = require('../../utils/functions').logging;
-    if (message.author.bot) return;
+async function getInvite(guildResult, inviteResponse, message, client) {
+	const embed = new Discord.MessageEmbed();
+	const logging = require('../../utils/functions').logging;
+	embed.setTitle(`Billybobbeep | Invite Links`);
+	embed.setTimestamp();
+	embed.setColor(guildResult.preferences ? guildResult.preferences.embedColor : '#447ba1');
+	embed.setDescription(
+		`${message.guild.id} does not allow you to send links to external servers.\n`+
+		`**Connected Server:** ${inviteResponse.guild.name}\n` +
+		`**Detected Invite Link:** https://discord.gg/${inviteResponse.code}`
+	);
+	await message.author.send(embed);
 
-    //Invite link detecting
-    let args = message.content.slice(prefix.length).trim().split(/ +/g);
-    embed.setTitle(`Billybobbeep | Invite Links`);
-    embed.setTimestamp();
-    embed.setColor(result.embedColor);
-    let inviteLinks = result.inviteLinks || false;
-    if (!inviteLinks) return;
+	embed.setTitle('Invite Link Detected');
+	embed.setDescription(
+		`**Server:** [${inviteResponse.guild.name}](https://discord.gg/${inviteResponse.code})\n` +
+		`**Message ID:** ${message.id}\n\n` +
+		`**Channel:** ${message.channel}\n` +
+		`**Channel ID:** ${message.channel.id}\n\n` +
+		`**Author:** ${message.author}\n` +
+		`**Author Tag:** ${message.author.tag}\n` +
+		`**Author ID:** ${message.author.id}\n`
+	)
 
-    if (message.guild.owner.id === message.author.id) return;
-    if (message.content.toLowerCase().includes('discord.gg') || message.content.toLowerCase().includes('discord.com/invite')) {
-      message.delete();
-      embed.setDescription(`We have detected that you have sent a discord invite link, if you would like to share your server inside of our server please contact one of the owners.\n` +
-        `Detected Invite Link: **${message.content.toLowerCase()}**\n` +
-        `Thanks, the ${message.guild.name} Staff Team`);
-      await message.author.send(embed);
+	logging(embed, message, client);
+}
+module.exports = (message, client) => {
+	if (!message.guild) return;
+	guildData.findOne({ guildId: message.guild.id }).then(result => {
+		if (message.author.bot) return;
+		let inviteLinks = result.preferences ? result.preferences.inviteLinks : false;
+		if (!inviteLinks) return;
 
-      embed.setTitle(`Invite Link Detected`);
-      embed.setDescription(`**Invite Link:** ${message.content.toLowerCase()}\n` +
-        `**Message ID:** ${message.id}\n\n` +
-        `**Channel:** ${message.channel}\n` +
-        `**Channel ID:** ${message.channel.id}\n\n` +
-        `**Author:** ${message.author}\n` +
-        `**Author Tag:** ${message.author.tag}\n` +
-        `**Author ID:** ${message.author.id}\n`)
-        
-      logging(embed, message, client);
-    }
-  });
+		//if (message.guild.owner.id == message.author.id) return;
+		if ((message.content).toLowerCase().includes(il)) {
+			message.delete();
+			(message.content).split(/ +/g).forEach(word => {
+				if (il.includes(word.toLowerCase())) {
+					if (il.includes(word.toLowerCase())) return;
+					axios.get(`https://discord.com/api/v9/invites/${word}`).then(data => {
+						if (data.data && !data.data.error)
+							getInvite(result, data.data, message, client);
+					});
+				}
+			});
+		}
+	});
 }
