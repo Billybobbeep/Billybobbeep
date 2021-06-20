a7df28c94f980122d9e18b579dd23ddd41c9fbf4
Tyler <tylerrp06@gmail.com>
2021-06-20 17:49:15 +0100

    Created a poll slash command
    Allowed ~poll to be used more accurately via slash commands

--- commands/fun/poll.js
+++ commands/fun/poll.js
@@ -1,53 +1,51 @@
 const Discord = require('discord.js');
-const configFile = require('../../utils/config.json');
 const guildData = require('../../events/client/database/models/guilds.js');
-
-//[Main letables]\\
-let title;
-let description;
-let color;
-let channel;
-let autherValid;
 
 module.exports = {
     name: 'poll',
     description: 'Send a poll',
     catagory: 'generator',
     guildOnly: true,
-    //options: [{ name: 'description', description: 'The poll description', type: 3, required: true }, { name: 'title', description: 'The poll title', type: 3, required: false }, { name: 'channel', description: 'The channel to send the poll in', type: 7, required: false }],
+    slashInfo: { enabled: true, public: true, options: { mod: true }},
+    options: [{ name: 'description', description: 'The poll description', type: 3, required: true }, { name: 'title', description: 'The poll title', type: 3, required: false }, { name: 'channel', description: 'The channel to send the poll in', type: 7, required: false }],
     /**
      * @param {object} message The message that was sent
      * @param {string} prefix The servers prefix
      * @param {objects} client The bots client
      */
-    execute (message, prefix, _client) {
+    execute (message, prefix, client) {
         if (!message.data) {
             guildData.findOne({ guildId: message.guild.id }).then(async result => {
                 let args = message.content.slice(prefix.length).trim().split(/ +/g);
-                let pollChannel = message.mentions.channels.first();
-                let pollDescription = args.slice(2).join(' ');
+                let channel = message.mentions.channels.first();
+                let desc = args.slice(2).join(' ');
 
-                let embedPoll = new Discord.MessageEmbed()
+                if (!channel) message.channel.send(`Usage: ${result.prefix}poll \`#channel\` [description]`);
+                if (!isNaN(channel)) channel = client.channels.fetch(channel);
+
+                const embed = new Discord.MessageEmbed()
                     .setTitle('New Poll!')
-                    .setDescription(pollDescription)
+                    .setDescription(desc)
                     .setColor(result.preferences ? result.preferences.embedColor : '#447ba1')
                     .setFooter(`Poll created by: ${message.author.tag}`)
-                let msgEmbed = await pollChannel.send(embedPoll);
-                await msgEmbed.react('👍');
-                await msgEmbed.react('👎');
+                let msg = await channel.send(embedPoll);
+                await msg.react('👍');
+                await msg.react('👎');
             });
         } else {
             guildData.findOne({ guildId: message.guild_id }).then(async result => {
-                let pollChannel = message.data.options.channel || message.channel_id
-
-                let embedPoll = new Discord.MessageEmbed()
-                    .setTitle(message.data.options.title || 'New Poll!')
-                    .setDescription(message.data.options.description)
+                let channel = message.data.options[2] ? message.data.options[2].value : message.channel_id;
+                if (!isNaN(channel)) channel = await client.channels.fetch(channel);
+                else channel = await client.channels.fetch(channel.id)
+                let embed = new Discord.MessageEmbed()
+                    .setTitle(message.data.options[1] ? message.data.options[1].value : 'New Poll!')
+                    .setDescription(message.data.options[0].value)
                     .setColor(result.preferences ? result.preferences.embedColor : '#447ba1')
-                    .setFooter(`Poll created by: ${message.author_tag}`)
-                let msgEmbed = await pollChannel.send(embedPoll);
-                await msgEmbed.react('👍');
-                await msgEmbed.react('👎');
+                    .setFooter(`Poll created by: ${message.member.user.username}#${message.member.user.discriminator}`)
+                require('../../utils/functions').slashCommands.reply(message, client, 'I have posted your poll');
+                let msg = await channel.send(embed);
+                await msg.react('👍');
+                await msg.react('👎');
             });
         }
     }
