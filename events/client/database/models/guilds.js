ddee8f0e7c3fe7ab0d39abc9765501b8be5142f8
Tyler <tylerrp06@gmail.com>
2021-07-31 18:49:36 +0100

    Updated guild schema and added comments
    Added comments for readers to gain an understanding of what each element is for

--- events/client/database/models/guilds.js
+++ events/client/database/models/guilds.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const guildSchema = new mongoose.Schema({
-    guildId: { type: String },
-    prefix: { type: String, default: '~' },
+    guildId: { type: String }, // The server ID
+    prefix: { type: String, default: '~' }, // The servers custom prefix
 
-    counting_number: { type: Number },
+    counting_number: { type: Number }, // The current number in the counting channel
 
     preferences: {
-        inviteLinks: { type: Boolean },
-        autoRoles: { type: Array },
-        lvlRoles: { type: Array },
-        embedColor: { type: String, default: '#447ba1' },
+        autoRoles: { type: Array }, // The roles to automatically give users upon joining
+        lvlRoles: { type: Array }, // Allows users to claim a certain role after achieving a certain level
+        embedColor: { type: String, default: '#447ba1' }, // The servers embed color
 
+        // Server Stats, voice channel ID's
         serverStats_totalNo: { type: String },
         serverStats_memberNo: { type: String },
         serverStats_botNo: { type: String },
@@ -19,12 +19,17 @@ const guildSchema = new mongoose.Schema({
         serverStats_memberNoText: { type: String },
         serverStats_botNoText: { type: String },
 
-        welcomeChannel: { type: String },
-        countingChannel: { type: String },
-        loggingChannel: { type: String },
+        welcomeChannel: { type: String }, // The servers welcome channel, welcomes and dismisses users
+        countingChannel: { type: String }, // The servers counting channel, if counting is enabled
+        loggingChannel: { type: String }, // The servers logging channel to log events
 
-        modRole: { type: String },
-        mutedRole: { type: String },
+        modRole: { type: String }, // The server mod role
+        mutedRole: { type: String }, // The role that muted users recieve to prevent them from talking
+
+        ecoEnabled: { type: Boolean }, // Enables and disables economy commands in the server
+        levelsEnabled: { type: Boolean }, // Enables and disables levelling in the server
+        inviteLinks: { type: Boolean }, // Automatically remove all third-party server invites
+        cleanFilter: { type: Boolean } // Prevent commands that may include explicit content from being executed
     }
 });
 
