b6fc30e292eed04282d79c972867fbb5591e313e
Tyler <tylerrp06@gmail.com>
2021-08-16 15:32:14 +0100

    Updated ban logging
    Fixed bugs & cleaned up code

--- events/backend/logging.js
+++ events/backend/logging.js
@@ -5,42 +5,42 @@ const logging = require('../../utils/functions').logging;
 
 module.exports.add = (guild, user, client) => {
     guildData.findOne({ guildId: guild.id }).then(result => {
-        const me = message.guild.members.cache.get(client.user.id);
+        const me = (await client.guilds.fetch(guild.id)).members.cache.get(client.user.id);
         if (
             !me.permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR) ||
             !me.permissions.has(Discord.Permissions.FLAGS.VIEW_AUDIT_LOG) &&
             !me.permissions.has(Discord.Permissions.FLAGS.SEND_MESSAGES)
         ) return;
         guild.fetchAuditLogs()
-        .then(logs => {
-            let ban = logs.entries
-            .filter(e => e.action === 'MEMBER_BAN_ADD')
-            .sort((a, b) => b.createdAt - a.createdAt)
-            .first();
-            
-            if (ban && ban.executor.id === client.user.id) return;
-            embed.setTitle('User Banned');
-            embed.setDescription(
-                `**User Tag:** ${user.tag}\n` +
-                `**User ID:** ${user.id}\n\n` +
-                `**Reason:** ${ban.reason || 'No reason was provided'}\n\n` +
-                `**Moderator:** ${ban ? ban.executor : 'Unknown'}\n` +
-                `**Moderator Tag:** ${ban ? ban.executor.tag : 'Unknown'}\n` +
-                `**Moderator ID:** ${ban ? ban.executor.id : 'Unknown'}`
-            )
-            embed.setTimestamp(ban.createdTimestamp);
-            embed.setColor(result && result.preferences ? result.preferences.embedColor : '#447ba1');
+            .then(logs => {
+                let ban = logs.entries
+                    .filter(e => e.action === 'MEMBER_BAN_ADD')
+                    .sort((a, b) => b.createdAt - a.createdAt)
+                    .first();
 
-            let loggingChannel = client.channels.cache.get(result && result.preferences ? result.preferences.loggingChannel : '');
-            if (loggingChannel)
-                loggingChannel.send({ embeds: [embed] }).catch(() => {return});
-        });
+                if (ban && ban.executor.id == client.user.id) return;
+                embed.setTitle('User Banned');
+                embed.setDescription(
+                    `**User:** ${user.tag}\n` +
+                    `**User ID:** ${user.id}\n\n` +
+                    `**Reason:** ${ban.reason || 'No reason was provided'}\n\n` +
+                    `**Moderator:** ${ban ? ban.executor : 'Unknown'}\n` +
+                    `**Moderator Tag:** ${ban ? ban.executor.tag : 'Unknown'}\n` +
+                    `**Moderator ID:** ${ban ? ban.executor.id : 'Unknown'}`
+                )
+                embed.setTimestamp(ban.createdTimestamp);
+                embed.setColor(result?.preferences ? result.preferences.embedColor : '#447ba1');
+
+                let loggingChannel = client.channels.cache.get(result?.preferences ? result.preferences.loggingChannel : '');
+                if (loggingChannel)
+                    loggingChannel.send({ embeds: [embed] }).catch(() => { return });
+            });
     });
 }
 
 module.exports.remove = (guild, user, client) => {
     guildData.findOne({ guildId: guild.id }).then(result => {
-        const me = message.guild.members.cache.get(client.user.id);
+        const me = (await client.guilds.fetch(guild.id)).members.cache.get(client.user.id);
         if (
             !me.permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR) ||
             !me.permissions.has(Discord.Permissions.FLAGS.VIEW_AUDIT_LOG) &&
@@ -48,32 +48,32 @@ module.exports.remove = (guild, user, client) => {
         ) return;
         setTimeout(() => {
             guild.fetchAuditLogs()
-            .then(logs => {
-                let ban = logs.entries
-                .filter(e => e.action === 'MEMBER_BAN_REMOVE')
-                .sort((a, b) => b.createdAt - a.createdAt)
-                .first();
-                let pb = logs.entries
-                .filter(e => e.action === 'MEMBER_BAN_ADD')
-                .filter(e => e.target.id === ban.target.id)
-                .sort((a, b) => b.createdAt - a.createdAt)
-                .first();
+                .then(logs => {
+                    let ban = logs.entries
+                        .filter(e => e.action == 'MEMBER_BAN_REMOVE')
+                        .sort((a, b) => b.createdAt - a.createdAt)
+                        .first();
+                    let pb = logs.entries
+                        .filter(e => e.action == 'MEMBER_BAN_ADD')
+                        .filter(e => e.target.id === ban.target.id)
+                        .sort((a, b) => b.createdAt - a.createdAt)
+                        .first();
 
-                embed.setTitle('User Unbanned');
-                embed.setDescription(
-                `**User Tag:** ${ban.target.tag}\n` +
-                `**User ID:** ${ban.target.id}\n\n` +
-                `**Banned For:** ${pb ? pb.reason.toString() : 'No reason was provided'}\n\n` +
-                `**Moderator:** ${ban ? ban.executor : 'Unknown'}\n` +
-                `**Moderator Tag:** ${ban ? ban.executor.tag : 'Unknown'}\n` +
-                `**Moderator ID:** ${ban ? ban.executor.id : 'Unknown'}`
-                )
-                embed.setTimestamp(ban.createdTimestamp);
-                embed.setColor(result && result.preferences ? result.preferences.embedColor : '#447ba1');
-                let loggingChannel = client.channels.cache.get(result && result.preferences ? result.preferences.loggingChannel : '');
-                if (loggingChannel)
-                    loggingChannel.send({ embeds: [embed] }).catch(() => {return});
-            });
+                    embed.setTitle('User Unbanned');
+                    embed.setDescription(
+                        `**User:** ${ban.target.tag}\n` +
+                        `**User ID:** ${ban.target.id}\n\n` +
+                        `**Banned For:** ${pb ? pb.reason.toString() : 'No reason was provided'}\n\n` +
+                        `**Moderator:** ${ban ? ban.executor : 'Unknown'}\n` +
+                        `**Moderator Tag:** ${ban ? ban.executor.tag : 'Unknown'}\n` +
+                        `**Moderator ID:** ${ban ? ban.executor.id : 'Unknown'}`
+                    )
+                    embed.setTimestamp(ban.createdTimestamp);
+                    embed.setColor(result?.preferences ? result.preferences.embedColor : '#447ba1');
+                    let loggingChannel = client.channels.cache.get(result?.preferences ? result.preferences.loggingChannel : '');
+                    if (loggingChannel)
+                        loggingChannel.send({ embeds: [embed] }).catch(() => { return });
+                });
         }, 100);
     });
 }
