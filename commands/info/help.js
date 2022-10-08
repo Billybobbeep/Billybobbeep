2d77abf926c714ae417473fc2f30cdf6d4273976
Tyler <tylerrp06@gmail.com>
2022-10-08 11:53:03 +0100

    Created help command
    Create a help command to display a quick menu to view our legal policies and redirect users to the '/commands' command

---
+++ commands/info/help.js
@@ -0,0 +1,42 @@
+const { Client, CommandInteraction, EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  name: "help",
+  description: "Get a general overview of the bot",
+  category: "info",
+  slashInfo: {
+    enabled: true,
+    public: false
+  },
+  /**
+   * Get the command's slash info
+   * @returns The slash information
+   */
+  getSlashInfo: function() {
+    const builder = new SlashCommandBuilder();
+    // Set basic command information
+    builder.setName(this.name);
+    builder.setDescription(this.description);
+    // If the command can be used in DMs
+    builder.setDMPermission(false);
+    // Return the information in JSON format
+    return builder.toJSON();
+  },
+  /**
+   * Execute the selected command
+   * @param {CommandInteraction} interaction The interaction that was sent
+   * @param {Client} client The bots client
+   */
+  execute: function(interaction, client) {
+    const embed = new EmbedBuilder();
+
+    embed.setTitle("Billybobbeep | Help");
+    embed.setColor("#447ba1");
+    embed.setDescription([
+      "To view our commands, use /commands\n",
+      "To whom it may concern, we have a [Privacy Policy](https://docs.google.com/document/d/1bIQNkYiQf1aGLqGYqyjN5dV0cOwvrpJVUnhF7PSBcoI/edit?usp=sharing) and a [Terms of Use](https://docs.google.com/document/d/1rDoLsEn7Ad27fg-px8Sxhu3V1xCWH-TKdtOkHwB8a28/edit?usp=sharing) policy publicly available"
+    ].join("\n"));
+
+    interaction.reply({ embeds: [embed], ephemeral: false });
+  }
+}
